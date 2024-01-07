a40c20718e76a53976bf558aec28b69f2a80cdae
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2024-01-07 20:22:57 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 132ms, Memory: 25MB, Status: Passed - CodeTree

--- 240107/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
+++ 240107/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -1,22 +1,22 @@
 n1,n2 = map(int,input().split()) #a의 원소수, b의 원소수
-arr_a = list(map(int,input().split()))
-arr_b = list(map(int,input().split()))
+a = list(map(int,input().split()))
+b = list(map(int,input().split()))
 
-#연속하게 뽑았을 때 나올 수 있는 수열인지 판단하라
-def is_squence(arr_a, arr_b, n1, n2):
-    #b의 첫 원소 a의 위치를 찾자
-    pos = 0
-    for i in range(n1):
-        if(arr_a[i] == arr_b[0]):
-            pos = i
+#b수열이 완전히 일치하는지 확인해보자
+def is_same(n):
+    for i in range(n2):
+        if a[i+n] != b[i]:
+            return False
+    return True
 
-    #그다음 일치하는지
-    for j in range(n2):
-        if(arr_a[pos] != arr_b[j]):
-            return "No" #한번이라도 일치하지 않으면 false
-        pos+=1
-        
+#b가 a의 연속부분수열인지 확인해보자
+def is_subseq():
+    for i in range(n1-n2+1):
+        if is_same(i):
+            return True
+    return False
 
-    return "Yes"
-
-print(is_squence(arr_a, arr_b, n1, n2))
+if is_subseq():
+    print("Yes")
+else:
+    print("No")
