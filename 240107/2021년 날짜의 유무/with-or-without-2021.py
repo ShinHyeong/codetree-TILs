6a37fa54df4051b58bc25693d4cbfc2b056ab937
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2024-01-07 20:30:31 +0900

    Title: 2021년 날짜의 유무, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240107/2021년 날짜의 유무/with-or-without-2021.py
+++ 240107/2021년 날짜의 유무/with-or-without-2021.py
@@ -3,24 +3,24 @@ m,d = map(int,input().split())
 #1, 3, 5, 7, 8, 10, 12
 def is_exist(m,d):
     #상반기
-    for i in range(1, 7+1, 2):
-        if i==m and d<=31:
+    for i1 in range(1, 7+1, 2):
+        if i1==m and d<=31:
             return True
     
-    if i==2 and d<=28:
+    if m==2 and d<=28:
         return True
 
-    for i in range(4, 7+1, 2):
-        if i==m and d<=30:
+    for i2 in range(4, 7+1, 2):
+        if i2==m and d<=30:
             return True
 
     #하반기
     for j in range(8, 12+1, 2):
-        if i==m and d<=31:
+        if j==m and d<=31:
             return True
 
-    for j in range(9, 12+1, 2):
-        if i==m and d<=30:
+    for k in range(9, 12+1, 2):
+        if k==m and d<=30:
             return True
     
     return False
