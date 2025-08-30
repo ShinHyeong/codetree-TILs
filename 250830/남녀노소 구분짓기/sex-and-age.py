5a5c412ecf5cef1cd20b0f1cf26c7ce6a43963a2
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-08-30 16:13:38 +0900

    Title: 남녀노소 구분짓기, Time: 74ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250830/남녀노소 구분짓기/sex-and-age.py
@@ -0,0 +1,14 @@
+sex = int(input())
+
+#if1 : 성인인지
+#if2 : 성별
+if age >= 19:
+    if sex == 0:
+        print("MAN")
+    else:
+        print("WOMAN")
+else:
+    if sex == 0:
+        print("BOY")
+    else: 
+        print("GIRL")
