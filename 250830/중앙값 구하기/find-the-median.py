82e5842b2c6ccff86751d861200307b9863cf0e2
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-08-30 16:29:13 +0900

    Title: 중앙값 구하기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250830/중앙값 구하기/find-the-median.py
+++ 250830/중앙값 구하기/find-the-median.py
@@ -3,16 +3,18 @@ a = arr[0]
 b = arr[1]
 c = arr[2]
 
-if a<b:
-    if b<c:
-        print(b) #a b c
-    else: #b>c
-        if c<a: 
-            print(a) #c a b
-        else: #c>a 
-            print(c) #a c b
-else: #b<a
+#a, b 위치 케이스 -> c 위치를 샌드위치처럼 끼우기
+if (a>b): #b a
     if c<b:
-        print(b) #c b a
-    else: #c>b
-        print(c) # b c a
+        print(b)  # c b a
+    if b<c and c<a:
+        print(c) # b c a 
+    if a<c:
+        print(a) # b a c
+else: # a b
+    if c<a:
+        print(a) # c a b
+    if a<c and c<b:
+        print(c) # a c b
+    if b<c:
+        print(b) # a b c
