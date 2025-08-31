18d2ecc14c8ce222d7c011202c4912a7d57c3760
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-08-31 12:25:29 +0900

    Title: 문자열의 특정 위치 찾기 2, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250831/문자열의 특정 위치 찾기 2/find-specific-location-in-spring-2.py
@@ -0,0 +1,17 @@
+#영문자 입력받기
+s = input()
+#5개의 문자열 초기화
+arr = ["apple","banana","grape","blueberry","orange"]
+
+cnt=0
+
+#문자열 돌면서
+#3번째나 네번째 문자와 일치하는지 확인
+#cnt에 더하기
+for x in arr:
+    if s in x[2] or s in x[3]:
+        print(x)
+        cnt+=1
+
+#cnt 출력
+print(cnt)
