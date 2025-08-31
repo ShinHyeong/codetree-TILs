bbde88fe75d8bca67f8508e87c3bd67655fbf434
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-08-31 10:56:07 +0900

    Title: a로 채워넣기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250831/a로 채워넣기/filling-with-a.py
+++ 250831/a로 채워넣기/filling-with-a.py
@@ -1,9 +1,9 @@
 #문자열 받아주고
-#변화1 = 앞에서 2번째 원소가 뭔지 알아내고(index) a로 대체(replace)
-#변화2 = 뒤에서 2번째 원소가 뭔지 알아내고(index) a로 대체(replace)
+#변화1 = 앞에서 2번째 원소가 뭔지 알아내고(index) a로 대체(replace) 처음만
+#변화2 = 뒤에서 2번째 원소가 뭔지 알아내고(index) a로 대체(replace) 처음만
 #출력
 
 s = input()
-new1 = s.replace(s[1], "a")
-new2 = new1.replace(new1[-2], "a")
+new1 = s.replace(s[1], "a", 1)
+new2 = new1.replace(new1[-2], "a", 1)
 print(new2)
