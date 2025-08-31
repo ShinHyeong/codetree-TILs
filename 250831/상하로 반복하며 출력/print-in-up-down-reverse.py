8ff93b78f46e8c619874f8f24c1a1338d30b2f06
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-08-31 22:37:04 +0900

    Title: 상하로 반복하며 출력, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250831/상하로 반복하며 출력/print-in-up-down-reverse.py
@@ -0,0 +1,23 @@
+#정사각형의 크기 입력받기
+#0으로 된 정사각형 만들기
+n=int(input())
+arr = [[0 for _ in range(n)] for _ in range(n)]
+
+#배열 돌고
+for i in range(n):
+    start=1 #각 열마다 start는 1로 초기화
+    #짝수열인지 검사 : 짝수면 순서대로(위->아래), 홀수면 역방향으로(아래->위) 증가로 채우기
+    if i%2==0:
+        for j in range(n):
+            arr[j][i]=start
+            start+=1
+    else:
+        for j in range(n-1,-1,-1):
+            arr[j][i]=start
+            start+=1
+
+#배열 쭉 돌면서 출력
+for i in range(n):
+    for j in range(n):
+        print(arr[i][j],end="")
+    print()
