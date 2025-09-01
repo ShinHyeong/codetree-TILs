e975b3dc0d75667f026e90723b735002d94b515f
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-09-01 12:44:10 +0900

    Title: 숫자로 이루어진 사각형, Time: 66ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 250901/숫자로 이루어진 사각형/rectangle-with-a-number.py
@@ -0,0 +1,16 @@
+# n을 입력받고
+n = int(input())
+# 함수 작성 - 매개변수 : n / 행위 : 일의 자리로 이루어진 정사각형 출력
+def square(n):
+    arr = [[0 for _ in range(n)] for _ in range(n)]
+    start = 1 #시작하는 수
+    for i in range(n):
+        for j in range(n):
+            arr[i][j] = start
+            print(arr[i][j],end=' ')
+            start+=1
+            if start%10==0:
+                start = 1
+        print()
+# 함수 실행
+square(n)
