531b6a1653c52b2fea2023e36cc2318775b3a3f7
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-09-02 22:57:06 +0900

    Title: 두 수의 거듭제곱, Time: 40ms, Memory: 16MB, Status: Passed - Codetree

--- 250902/두 수의 거듭제곱/two-numbers-of-squares.py
+++ 250902/두 수의 거듭제곱/two-numbers-of-squares.py
@@ -1,10 +1,11 @@
 a, b = map(int, input().split())
 
 # Please write your code here.
-def square_ab(a,b):
-    multiple = a
-    for _ in range(b-1): #b-1번 a를 곱함
-        a *= multiple
-    return a
+def power(a,b): #a^b
+    cnt = 1
+    for _ in range(b): #b번 a를 곱함
+        cnt *= a
+    
+    return cnt
 
-print(square_ab(a,b))
+print(power(a,b))
