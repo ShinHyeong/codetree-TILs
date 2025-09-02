5eda64891a3e1dc7fea9a4a909f9efdc03630487
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-09-02 14:13:39 +0900

    Title: 짝수이면서 합이 5의 배수인 수, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250902/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.py
+++ 250902/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.py
@@ -1,6 +1,9 @@
 n = int(input())
 
 # Please write your code here.
+# n이 짝수인지 확인하는 함수 작성
+def is_even(n):
+    return n%2==0
 # 각자리 숫자합이 5의 배수인지 확인하는 함수 작성
 def multiple_5(n):
     sum_val = (n%10)+(n- n%10)//10
@@ -8,7 +11,7 @@ def multiple_5(n):
     return sum_val%5==0
 
 #if 문에 함수 넣고 print
-if multiple_5(n):
+if is_even(n) and multiple_5(n):
     print("Yes")
 else:
     print("No")
