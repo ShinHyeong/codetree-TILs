aaf0acbc9f3a8b83bf7709f2f6be8cd96f2bfa07
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-09-02 23:36:19 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 72ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250902/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -0,0 +1,22 @@
+n1, n2 = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+# Please write your code here.
+
+#b의 첫번째 원소를 a에서 어느위치에 있는지 확인
+#b의 처음~끝까지 돌면서 a도 그 인덱스부터 같이 비교하면서 돌았는데 한번도 어긋남없이 같다면 연속부분수열임
+def is_subSeq(a,b):
+    firstElementIndexInListA = a.index(b[0])
+
+    for i in range(n2):
+        #print(b[i], a[firstElementIndexInListA+i])
+        if b[i]!=a[firstElementIndexInListA+i]:
+            return False
+    return True
+
+#yes, no출력
+if is_subSeq(a,b):
+    print("Yes")
+else:
+    print("No")
