ceebef63c86e8e415a2b7b89cde71b9a1c78b3f5
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-09-02 22:23:30 +0900

    Title: 그 계절, 그 날, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250902/그 계절, 그 날/that-season-that-day.py
+++ 250902/그 계절, 그 날/that-season-that-day.py
@@ -27,12 +27,10 @@ def season(M):
         return "Winter"
 
 ##(2월 제외)y/m/d 날짜가 존재하는지 확인하는 함수 작성
-#m: 1,3,5,7,8,10,12-> d: 31
-#m: 4,6,9,11 -> d: 30
+#m: 1,3,5,7,8,10,12-> d: 1~31
+#m: 4,6,9,11 -> d: 1~30
 def is_valid_date(M,D):
     if 1<=M<=7:
-        if M==2:#2월은 그냥 넘김
-            return True 
         if M%2!=0: #odd M. : 1,3,5,7
             return 1<=D<=31
         else: #even M. : 2,4,6
@@ -43,19 +41,28 @@ def is_valid_date(M,D):
         else: #even M : 8,10,12
             return 1<=D<=31
 
-##(2월 제외) y/m/d 날짜가 존재하는지 확인
+## (2월 제외)y/m/d 날짜가 존재하는지 확인
 if is_valid_date(M,D):
 ##1. 존재한다면 : 월에 따라 계절 판별 후 출력
-#2월의 경우 한번더 검토 -> y가 윤년일 경우 계절 출력
-    if M==2: 
-        if is_leapYear(Y):
-            print(season(M))
-        else:
-            print(-1)
-# 2월이 아닌경우 계절 출력
+    if M==2:  #1) 2월의 경우 한번더 검토 -> y가 윤년인지 검사
+        if is_leapYear(Y): #윤년이라면 D:1~29인지 검사 -> 맞으면 계절 출력
+            #print("is leap year")
+            if 1<=D<=29:
+                print(season(M))
+            else:
+                print(-1)
+        else: #윤년이 아니라면 D:1~28인지 검사 -> 맞으면 계절 출력
+            #print("not leap year")
+            if 1<=D<=28:
+                print(season(M))
+            else:
+                print(-1)
+#2) 2월이 아닌경우 계절 출력
     else:
+        #print("not Feb, but valid date")
         print(season(M))
 
 ##2. 해당날짜 자체가 존재하지 않는다면 : -1 출력
 else:
+    #print("invalid date")
     print(-1)
