8227a50e0da07985779c5f9587a0894da643a31a
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-09-04 17:35:05 +0900

    Title: 나누고 빼면서 합하기, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250904/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
+++ 250904/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
@@ -4,12 +4,11 @@ A = list(map(int, input().split()))
 # Please write your code here.
 def process(m):
     sum_val=0
-    while m>=1:
-        #print(m, A[m-1])
-        sum_val += A[m-1]
-        if m%2!=0:
+    while m>=1: #M이 1이 될떄까지
+        sum_val += A[m-1] #A의 M번째 원소를 계속더하는데
+        if m%2!=0: #M=홀수면 M에서 1을 뺴고
             m-=1
-        else:
+        else: #M=짝수면 M에서 2을 나누고
             m//=2
     return sum_val
 print(process(m))
