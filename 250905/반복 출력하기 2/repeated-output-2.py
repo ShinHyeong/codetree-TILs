9eced40c72b92be037d02e720d6f8812c98d7407
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-09-05 20:47:41 +0900

    Title: 반복 출력하기 2, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250905/반복 출력하기 2/repeated-output-2.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+# Please write your code here.
+# 재귀함수 작성 : "HelloWorld"를 n번 출력하는 함수
+def print_text(n):
+    #종료조건 n==0 / 동작종료
+    if n==0:
+        return
+    
+    print_text(n-1)
+    print("HelloWorld")
+
+print(n)
