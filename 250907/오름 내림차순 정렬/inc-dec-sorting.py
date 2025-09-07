598d1831ef32ccc985dddcf438f189e0f5dd6ee0
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-09-07 15:40:48 +0900

    Title: 오름 내림차순 정렬, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 250907/오름 내림차순 정렬/inc-dec-sorting.py
+++ 250907/오름 내림차순 정렬/inc-dec-sorting.py
@@ -3,6 +3,11 @@ nums = list(map(int, input().split()))
 
 # Please write your code here.
 nums.sort() #오름차순 정렬
-print(nums) #출력
+for ele in nums:
+    print(ele, end=' ') #출력
+
+print()
+
 nums.sort(reverse=True) #내림차순 정렬
-print(nums) #출력
+for ele in nums:
+    print(ele, end=' ') #출력
