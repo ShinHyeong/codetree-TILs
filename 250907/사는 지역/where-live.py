fceedf5d9f9848a6744978f88f1c78757d12f938
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-09-07 17:13:23 +0900

    Title: 사는 지역, Time: 50ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250907/사는 지역/where-live.py
@@ -0,0 +1,36 @@
+n = int(input())
+name = []
+street_address = []
+region = []
+
+for _ in range(n):
+    n_i, s_i, r_i = input().split()
+    name.append(n_i)
+    street_address.append(s_i)
+    region.append(r_i)
+
+# Please write your code here.
+#사람 자료 저장하는 클래스
+class User:
+    def __init__(self, name, street_address, region):
+        self.name = name
+        self.addr = street_address
+        self.city = region
+
+#n개의 객체 저장하는 리스트
+users=[]
+for i in range(n):
+    users.append(User(name[i], street_address[i], region[i]))
+
+#리스트 처음~끝까지 사전순으로 이름이 가장 느린 사람의 자료 찾기 
+max_idx = 0
+max_name = "a"
+for i in range(n):
+    if users[i].name > max_name:
+        max_name = users[i].name
+        max_idx = i
+
+#그 사람 인덱스를 기반으로 정보 출력
+print("name",users[i].name)
+print("addr",users[i].addr)
+print("city",users[i].city)
