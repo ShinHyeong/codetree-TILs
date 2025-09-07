48041fb88622fd6db7ff74143a0b21135dd32e3f
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-09-07 17:47:38 +0900

    Title: 비오는 날, Time: 50ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250907/비오는 날/rainy-day.py
+++ 250907/비오는 날/rainy-day.py
@@ -11,26 +11,21 @@ for _ in range(n):
 
 # Please write your code here.
 # 미래 기상 데이터 클래스
-class Future:
+class Forecast:
     def __init__(self, date, day, weather):
         self.date =date
         self.day = day
         self.weather = weather
 
 # n개의 기상 데이터 클래스 저장한 리스트
-futures = [Future(date[i], day[i], weather[i]) for i in range(n)]
+forecast = [Forecast(date[i], day[i], weather[i]) for i in range(n)]
 
 # 리스트 처음~끝까지 하나씩 꺼내면서 비가 오는데 가장 빠른 날짜 찾기
-target_idx = 0
+ans = Forecast("9999-99-99","","")
 for i in range(n):
-    if futures[i].weather == "Rain":
-        target_idx = i
-        break
-
-for i in range(n):
-    if futures[i].weather == "Rain":
-        if futures[i].date < futures[target_idx].date:
+    if forecast[i].weather == "Rain":
+        if forecast[i].date < ans.date:
             target_idx = i
 
 #날짜, 요일, 날씨 포맷 출력
-print(futures[target_idx].date, futures[target_idx].day, futures[target_idx].weather)
+print(forecast[target_idx].date, forecast[target_idx].day, forecast[target_idx].weather)
