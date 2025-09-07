c9c87086765af051099da62751bc2f748b78bf84
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-09-07 16:25:20 +0900

    Title: 중앙값 계산 2, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250907/중앙값 계산 2/get-median-2.py
+++ 250907/중앙값 계산 2/get-median-2.py
@@ -2,7 +2,7 @@ n = int(input())
 arr = list(map(int, input().split()))
 
 # Please write your code here.
-# 중앙값 출력하는 함수
+# 지금까지 입력 받은 값의 중앙값 출력하는 함수 (오름차순으로 정렬했을 때 가장 중앙에 위치한값 출력)
 def get_mid(i):
     sample = arr[:i+1]
     # ex. 5개의 원소 0 1 2 3 4-> index 2
@@ -10,7 +10,7 @@ def get_mid(i):
 
 # 순서대로 정수를 읽다가
 # 홀수번쨰 원소이면
-# 지금까지 입력 받은 값의 중앙값 출력 (오름차순으로 정렬했을 때 가장 중앙에 위치한값 출력)
+# 지금까지 입력 받은 값의 중앙값 출력 
 for i in range(n):
     if i%2==0:
         print(get_mid(i),end=' ')
