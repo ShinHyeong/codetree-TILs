b2534c1512b50007574094ca523e0fbf99f39347
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-09-07 17:03:07 +0900

    Title: 폭탄 해제, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250907/폭탄 해제/bomb-removal.py
@@ -0,0 +1,18 @@
+unlock_code, wire_color, seconds = input().split()
+seconds = int(seconds)
+
+# Please write your code here.
+# 폭탄정보 (해체코드, 선의 색, 특정 초)
+class Bomb:
+    def __init__(self,unlock_code,wire_color,seconds):
+        self.uc = unlock_code
+        self.wc = wire_color
+        self.sc = seconds
+
+# 폭탄정보 입력        
+b1 = Bomb(unlock_code,wire_color,seconds)
+
+# 폭탄정보 출력
+print("code : {b1.uc}")
+print("color : {b1.wc}")
+print("second : {b1.sc}")
