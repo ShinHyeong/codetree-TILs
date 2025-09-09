e4e4fc8da76c7a493f4c9e0ede3900f400221116
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-09-09 19:09:43 +0900

    Title: 정렬된 수 위치 알아내기, Time: 55ms, Memory: 17MB, Status: Passed - Codetree

--- 250909/정렬된 수 위치 알아내기/indices-of-sorted-array.py
+++ 250909/정렬된 수 위치 알아내기/indices-of-sorted-array.py
@@ -3,9 +3,9 @@ sequence = list(map(int, input().split()))
 
 # Please write your code here.
 
-# ex. 정렬 전(nums) : [(1,8), (2,3), (3,5)] / 인덱스:pos(위치별) , 내용:value
-# -> 정렬 후(nums) : [(2,3), (3,5), (1,8)] / 인덱스:rank(등수별), 내용:value
-# 위치별 등수 저장하는 리스트 : [3, 1, 2] / 인덱스: pos, 내용:rank
+# ex. 정렬 전(indexed_sequence) : [(1,8), (2,3), (3,5)] / 인덱스:pos(위치별) , 내용:value
+# -> 정렬 후(indexed_sequence) : [(2,3), (3,5), (1,8)] / 인덱스:rank(등수별), 내용:value
+# 위치별 등수 저장하는 리스트(ranks) : [3, 1, 2] / 인덱스: pos, 내용:rank
 
 # 위치와 내용도 함께 저장하는 리스트
 indexed_sequence = list(enumerate(sequence))
