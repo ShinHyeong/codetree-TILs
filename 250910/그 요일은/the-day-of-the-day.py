f6c3c32c3d43def90cc3f20c1de9f1b6b132dc6b
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-09-10 07:27:09 +0900

    Title: 그 요일은, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250910/그 요일은/the-day-of-the-day.py
+++ 250910/그 요일은/the-day-of-the-day.py
@@ -4,11 +4,11 @@ A = input()
 # Please write your code here.
 
 # [0, 1월, 2월, 3월, 4월, 5월, 6월, 7월, 8월, 9월, 10월, 11월, 12월] 에 해당하는 일수
-nums_of_day = [0,31,28,31,30,31,30,31,31,30,31,30,31]
+nums_of_day = [0,31,29,31,30,31,30,31,31,30,31,30,31]
 day_of_week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 
-#m1/d1~m2/d2까지 총 며칠이 되는지 구하고 
-elapsed_day = (sum(nums_of_day[:m2])+d2) - (sum(nums_of_day[:m1])+d1) + 1
+#m1/d1~m2/d2까지 흐른 일수 구하고 
+elapsed_day = (sum(nums_of_day[:m2])+d2) - (sum(nums_of_day[:m1])+d1)
 
 #현재 요일 = 월요일
 curr_idx = 1
@@ -17,9 +17,8 @@ target_idx = day_of_week.index(A)
 # target요일(=a) 등장횟수 초기화
 target_cnt = 0
 
-# m1/d1~m2/d2 까지의 총 일수동안
+# m1/d1~m2/d2 까지 흐른 일수동안
 for _ in range(elapsed_day):
-
 #현재요일에서 하나씩 이동하는데
     curr_idx += 1
 #단, 현재요일 인덱스가 7이되면 0으로 초기화해줌    
