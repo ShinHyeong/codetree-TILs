f74aca075c94fc6aeaaafb708e292d0175df5a67
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-09-09 19:47:43 +0900

    Title: DateTime to DateTime, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250909/DateTime to DateTime/datetime-to-datetime.py
+++ 250909/DateTime to DateTime/datetime-to-datetime.py
@@ -5,15 +5,17 @@ a, b, c = map(int, input().split())
 def is_slow(a,b,c):
     if a<11:
         return False
-    if b<11:
+    
+    if a==11 and b<11:
         return False
-    if c<11:
+    
+    if a==11 and b==11 and c<11:
         return False
+        
     return True
 
 # A일 B시 C분이 11일 11시 11분보다 더 앞서는지 확인
 if is_slow(a,b,c): #앞서지 않음을 확인하면 몇분 걸리는지 계산
     print((a*24*60+b*60+c) - (11*24*60+11*60+11))
-
 else: #A일 B시 C분이 11일 11시 11분보다 더 앞선다면 −1을 출력
     print(-1)
