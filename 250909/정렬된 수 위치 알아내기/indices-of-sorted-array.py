634b8919c333b5af0017c78ffda96010efcb535c
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-09-09 19:09:04 +0900

    Title: 정렬된 수 위치 알아내기, Time: 56ms, Memory: 16MB, Status: Passed - Codetree

--- 250909/정렬된 수 위치 알아내기/indices-of-sorted-array.py
+++ 250909/정렬된 수 위치 알아내기/indices-of-sorted-array.py
@@ -7,19 +7,19 @@ sequence = list(map(int, input().split()))
 # -> 정렬 후(nums) : [(2,3), (3,5), (1,8)] / 인덱스:rank(등수별), 내용:value
 # 위치별 등수 저장하는 리스트 : [3, 1, 2] / 인덱스: pos, 내용:rank
 
-# 위치와 내용도 함께 저장하는 리스트 (위치는 1부터 시작이니 +1 해줌)
-values = [(i+1, sequence[i]) for i in range(n)]
+# 위치와 내용도 함께 저장하는 리스트
+indexed_sequence = list(enumerate(sequence))
 
-# 랭킹 저장하는 리스트 : ex. 1번원소(=8)->정렬후 5번째로 감 / 인덱스:pos, 내용:rank
-ranks = [0]*(n+1)
+# 랭킹 저장하는 리스트
+ranks = [0]*n
 
 # 내용으로 오름차순 정렬
-values.sort(key=lambda v : v[1])
+indexed_sequence.sort(key=lambda s : s[1])
 
-# 각 번호 위치에 랭킹 저장 (랭킹은 1부터 시작이니 +1 해줌)
-for rank, (pos, value) in enumerate(values):
+# 각 번호 위치에 랭킹 저장 (랭킹은 1부터 시작이니 +1 처리해줌)
+for rank, (pos, value) in enumerate(indexed_sequence):
     ranks[pos] = rank+1
 
-# 공백을 사이에 두고 출력
-for ele in ranks[1:]:
-    print(ele,end=" ")
+# 공백을 사이에 두고 출력 
+for r in ranks:
+    print(r,end=" ")
