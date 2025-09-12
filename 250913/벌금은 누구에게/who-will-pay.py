83d7f014e6a607d4ff2b6255bf1ad2726db82aaa
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-09-13 05:27:47 +0900

    Title: 벌금은 누구에게, Time: 64ms, Memory: 16MB, Status: Passed - Codetree

--- 250913/벌금은 누구에게/who-will-pay.py
+++ 250913/벌금은 누구에게/who-will-pay.py
@@ -5,20 +5,14 @@ student = [int(input()) for _ in range(M)]
 # 학생번호별로 벌칙에 걸린 횟수를 저장하는 리스트 (idx:학생번호 / value:벌칙에걸린횟수)
 penalty_counts = [0] * (N+1) #0번은 학생번호로 취급안하므로 idx=0의 value는 0으로 남겨두고, idx=1부터 적용
 
+#최초로 벌금을 내게 되는 학생
+first_over_k = -1
+
 # 학생번호가 m번에 걸쳐 주어질 때마다
 for i in student:
     penalty_counts[i] += 1 #해당 번호의 벌칙받은횟수+1
+    if penalty_counts[i] >= K:
+        first_over_k = i
+        break
 
-# 학생번호별로 벌칙에 걸린 횟수를 확인하면서
-# 만약 그 횟수(=리스트의 value)가 k 이상이라면 학생번호(=리스트의 idx) 출력
-# 없으면 -1 출력
-
-def over_k(penalty_counts, k):
-    for i, cnt in enumerate(penalty_counts):
-        if cnt >= k:
-            return i
-    return -1
-
-print(over_k(penalty_counts, K))
-
-
+print(first_over_k)
