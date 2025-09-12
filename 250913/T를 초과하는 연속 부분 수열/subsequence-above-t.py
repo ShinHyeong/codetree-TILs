058d8c12ee78e6a09e3e12e25c75037920208165
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-09-13 03:51:13 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250913/T를 초과하는 연속 부분 수열/subsequence-above-t.py
+++ 250913/T를 초과하는 연속 부분 수열/subsequence-above-t.py
@@ -2,24 +2,16 @@ n, t = map(int, input().split())
 arr = list(map(int, input().split()))
 
 # Please write your code here.
-# T보다 큰 수로만 이루어진 연속 부분 수열의 길이를 저장하는 리스트
-over_T_lengths = []
-
 max_length=0
-curr_seq_length=1 if arr[0] > t else 0
+curr_seq_length=0
 
-for i in range(1, n): #idx=1부터 n-1까지 T보다 큰지 검사 
-    if arr[i]>t and arr[i-1]>t : #T보다 큰 원소가 연속적이다 (현재원소도 큰데 직전원소도 크다)
-        if curr_seq_length==0: #그런데 처음 카운트했다
-            curr_seq_length = 1 #1로 초기화 
-        curr_seq_length+=1
-    else: #그렇지 않다
-        if arr[i]>t: #그렇지만 현재 원소는 T보다 크다
-            curr_seq_length=1
-            max_length = max(curr_seq_length, max_length)
-        else:
-            max_length = max(curr_seq_length, max_length)
-            curr_seq_length=0
-max_length = max(curr_seq_length, max_length)
-        
+for i in range(n): #idx=0부터 n-1까지 돌면서
+    if arr[i] > t: #현재 원소 > T인 경우
+        curr_seq_length += 1
+    else: #현재 원소가 <= T인 경우
+        curr_seq_length = 0
+    
+    #최대길이 업데이트    
+    max_length = max(curr_seq_length, max_length)
+
 print(max_length)
