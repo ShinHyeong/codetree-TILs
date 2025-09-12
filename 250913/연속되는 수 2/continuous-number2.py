30d4073df90c006595d9b951d306856f77bc99e5
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-09-13 02:44:11 +0900

    Title: 연속되는 수 2, Time: 75ms, Memory: 19MB, Status: Runtime Error - Codetree

--- 250913/연속되는 수 2/continuous-number2.py
+++ 250913/연속되는 수 2/continuous-number2.py
@@ -7,13 +7,15 @@ arr = [int(input()) for _ in range(n)]
 consecutive_counts = []
 
 # 다른숫자가 나왔는지 구분하는 법 : i==0 이거나 직전원소가 다른경우(a[i]!=a[i-1]
-cnt=0
-for i in range(n):
-    if i==0 or arr[i]!=arr[i-1]:#다른숫자가 나왔다
+cnt=1 #i==0 인 경우부터 시작이니까
+
+for i in range(1,n):
+    if arr[i]!=arr[i-1]:#다른숫자가 나왔다
         consecutive_counts.append(cnt) #직전숫자 연속해서 나온 횟수를 리스트에 추가해줌
         cnt=0 #현재숫자 횟수 초기화
         cnt+=1 #현재숫자 횟수 카운트 시작
     else: #연속한다
         cnt+=1 #현재숫자 횟수 카운트
 
-print(max(consecutive_counts))
+# 연속해서 동일한 숫자가 나오는 횟수 중 최댓값 출력
+print(max(consecutive_counts)) 
