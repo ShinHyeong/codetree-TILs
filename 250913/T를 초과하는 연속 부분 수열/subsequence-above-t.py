bc27e54fb5faaccdca495fc20e5a11c1ed7f7983
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-09-13 03:47:05 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250913/T를 초과하는 연속 부분 수열/subsequence-above-t.py
+++ 250913/T를 초과하는 연속 부분 수열/subsequence-above-t.py
@@ -8,14 +8,18 @@ over_T_lengths = []
 max_length=0
 curr_seq_length=1 if arr[0] > t else 0
 
-for i in range(1, n): #idx=1부터 n-1까지 T보다 큰지 검사
+for i in range(1, n): #idx=1부터 n-1까지 T보다 큰지 검사 
     if arr[i]>t and arr[i-1]>t : #T보다 큰 원소가 연속적이다 (현재원소도 큰데 직전원소도 크다)
         if curr_seq_length==0: #그런데 처음 카운트했다
             curr_seq_length = 1 #1로 초기화 
         curr_seq_length+=1
     else: #그렇지 않다
-        max_length = max(curr_seq_length, max_length)
-        curr_seq_length=0 #길이 초기화
+        if arr[i]>t: #그렇지만 현재 원소는 T보다 크다
+            curr_seq_length=1
+            max_length = max(curr_seq_length, max_length)
+        else:
+            max_length = max(curr_seq_length, max_length)
+            curr_seq_length=0
 max_length = max(curr_seq_length, max_length)
         
 print(max_length)
