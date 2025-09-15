aababc34159f6ff95bb86e16e0c3b1c24df50c20
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-09-15 21:53:23 +0900

    Title: 방향에 맞춰 이동, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250915/방향에 맞춰 이동/move-in-direction.py
@@ -0,0 +1,27 @@
+n = int(input())
+moves = [tuple(input().split()) for _ in range(n)]
+dir = [move[0] for move in moves]
+dist = [int(move[1]) for move in moves]
+
+# Please write your code here.
+
+x, y = 0, 0 #시작위치
+dir_num = 0
+#순서대로 동 남 서 북
+dx, dy = [1, 0, -1, 0], [0, -1, 0, 1]
+
+for k in range(n):
+    if dir[k]=="E":
+        dir_num = 0
+    if dir[k]=="S":
+        dir_num = 1
+    if dir[k]=="W":
+        dir_num = 2
+    if dir[k]=="N":
+        dir_num = 3
+    
+    for _ in range(dist[k]):
+        x += dx[dir_num]
+        y += dy[dir_num]
+
+print(x,y)
