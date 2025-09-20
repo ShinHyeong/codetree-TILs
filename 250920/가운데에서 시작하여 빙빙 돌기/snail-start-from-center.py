e1c13b59f069b6ea566f945d56a86fcb80966541
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-09-20 15:39:10 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 67ms, Memory: 17MB, Status: Passed - Codetree

--- 250920/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
+++ 250920/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
@@ -2,25 +2,60 @@ n = int(input())
 grid = [[0] * n for _ in range(n)]
 
 # Please write your code here.
-x,y = n//2, n//2 #nxn 크기의 정사각형 가운데에서 시작
-grid[x][y]=1 #1부터 시작
-dir_num =0 #이동하는 방향 초기화
 
 #동->북->서->남 : x,y변화량
 dxs,dys = [0,-1,0,1],[1,0,-1,0]
 
-#주어진 x,y가 격자 범위내에 있는가
-def in_range(x,y):
-    return (0<=x and x<n) and (0<=y and y<n)
+#완성된 사각형 출력
+def print_rect():
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            print(grid[i][j], end=' ')
+        print()
 
-for i in range(2, n*n+1): #2~n*n 를 채워넣는다
-    nx,ny = x+dxs[dir_num],y+dys[dir_num] #이동이 예상되는 다음칸
-    if not(in_range(nx,ny) and grid[nx][ny]==0): #다음칸으로 이동못한다
-        dir_num = (dir_num+1)%4 #동->북->서->남 방향으로 회전
+### 로직 ###
+#n=3
+#현재칸에서
+#-> 이동방향 변경 :동쪽 -> (1칸 이동 -> 숫자를 채워넣는다) x1
+#-> 이동방향 변경 :북쪽 -> (1칸 이동 -> 숫자를 채워넣는다) x1
+#-> 이동방향 변경 :서쪽 -> (1칸 이동 -> 숫자를 채워넣는다) x2
+#-> 이동방향 변경 :남쪽 -> (1칸 이동 -> 숫자를 채워넣는다) x2
+#-> 이동방향 변경 :동쪽 -> (1칸 이동 -> 숫자를 채워넣는다) x(n-1)
 
-    x,y = x+dxs[dir_num],y+dys[dir_num] #다음칸으로 이동한다
+#n=5
+#현재칸에서
+#-> 이동방향 변경 :동쪽 -> (1칸 이동 -> 숫자를 채워넣는다) x1
+#-> 이동방향 변경 :북쪽 -> (1칸 이동 -> 숫자를 채워넣는다) x1
+#-> 이동방향 변경 :서쪽 -> (1칸 이동 -> 숫자를 채워넣는다) x2
+#-> 이동방향 변경 :남쪽 -> (1칸 이동 -> 숫자를 채워넣는다) x2
+#-> 이동방향 변경 :동쪽 -> (1칸 이동 -> 숫자를 채워넣는다) x3
+#-> 이동방향 변경 :북쪽 -> (1칸 이동 -> 숫자를 채워넣는다) x3
+#-> 이동방향 변경 :서쪽 -> (1칸 이동 -> 숫자를 채워넣는다) x4
+#-> 이동방향 변경 :남쪽 -> (1칸 이동 -> 숫자를 채워넣는다) x4
+#-> 이동방향 변경 :동쪽 -> (1칸 이동 -> 숫자를 채워넣는다) x(n-1)
 
-    grid[x][y]=i #숫자를 채워넣는다
+x,y = n//2, n//2 #nxn 크기의 정사각형 가운데에서 시작
+grid[x][y]=1 #1부터 채운다
+dir_num =0 #이동하는 방향 초기화 : 동
+
+i = 2
+step = 1
+
+while i <= n*n: #2~n*n 를 채워넣는다
+    if step==n:
+        step=n-1
+    for _ in range(2):
+        for _ in range(step):
+            x,y = x+dxs[dir_num],y+dys[dir_num] #다음칸으로 이동한다
+            grid[x][y]=i #숫자를 채워넣는다
+            i+=1
+            if i>n*n:
+                break
+        if i>n*n:
+                break
+        dir_num = (dir_num+1)%4 #동->북->서->남 으로 이동방향 변경
+    step+=1
+
 
 #완성된 사각형 출력
 for i in range(len(grid)):
