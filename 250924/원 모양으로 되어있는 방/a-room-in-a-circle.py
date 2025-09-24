c918d66914a5cac6fd46d44de73a221bf84f34f8
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-09-24 13:34:53 +0900

    Title: 원 모양으로 되어있는 방, Time: 73ms, Memory: 17MB, Status: Passed - Codetree

--- 250924/원 모양으로 되어있는 방/a-room-in-a-circle.py
+++ 250924/원 모양으로 되어있는 방/a-room-in-a-circle.py
@@ -8,8 +8,8 @@ a = [int(input()) for _ in range(n)]
 # 3번 : 6명 -> 6*(3-3) 
 # 4번 : 4명 -> 4*(4-3)
 # 0번 : 4명 -> 4*(5-3) #n=5
-# 1번 : 7명 -> 7*(6-3) #n-
-# 2번 : 8명 -> 8*(7-3) 
+# 1번 : 7명 -> 7*(6-3) #n=5
+# 2번 : 8명 -> 8*(7-3) #n=5
 
 #어떤 방에서 시작해야 각 방에 정해진 인원이 들어가는데까지의 거리의 합을 최소화할 수 있는지
 min_dist = sys.maxsize
@@ -20,7 +20,6 @@ for start_idx in range(n): #시작하는 방 하나 정하고
     for i in range(start_idx, start_idx+n):
         room_idx = i-n if i>n-1 else i
         dist += a[room_idx]*(i-start_idx)
-        #print(f"start: {start_idx}, room: {room_idx}, dist: {dist}")
     
     #최소값 계산
     min_dist=min(dist,min_dist)
