ca2f3a0737f158888c621b09808c37bf537a3047
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-09-27 06:27:40 +0900

    Title: 특정 수와 근접한 합, Time: 62ms, Memory: 17MB, Status: Passed - Codetree

--- 250927/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 250927/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -8,13 +8,9 @@ min_val = 10000
 for e1 in range(N-1):
     for e2 in range(e1+1,N):
         
-        sum_val=0        
-        #전체 숫자 중에
-        for i in range(N):    
-            if i==e1 or i==e2:
-                continue
-            sum_val += arr[i] #제외한 n-2개의 숫자들의 합 구하기
-    
+        # e1번과 e2번을 제외한 나머지 숫자의 총합
+        sum_val = sum(arr)-arr[e1]-arr[e2] 
+           
         #s와 (그 합의 차) 최솟값 구하기
         min_val = min(abs(S-sum_val),min_val)
 
