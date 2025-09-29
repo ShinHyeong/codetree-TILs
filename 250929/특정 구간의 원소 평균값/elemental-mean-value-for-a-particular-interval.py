9092f49683d75bb58f33751ee574a204e67154c0
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-09-29 22:17:43 +0900

    Title: 특정 구간의 원소 평균값, Time: 65ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 250929/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
+++ 250929/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
@@ -2,6 +2,7 @@ n = int(input())
 arr = list(map(int, input().split()))
 
 # Please write your code here.
+
 #구간의 시작점과 끝점을 잡아 완전탐색한다
 #평균값을 구한다
 #평균값이 그 구간의 원소 중 하나인지 확인한다
@@ -10,6 +11,7 @@ arr = list(map(int, input().split()))
 cnt=0
 for start_idx in range(n):
     for end_idx in range(start_idx+1, n):
+
         part = arr[start_idx:end_idx+1]
         sum_val = sum(part) 
         avg_val = sum_val//(len(part))
@@ -18,7 +20,6 @@ for start_idx in range(n):
         for ele in part:
             if ele == avg_val:
                 found = True
-                break
 
         if found==True:
             cnt+=1
