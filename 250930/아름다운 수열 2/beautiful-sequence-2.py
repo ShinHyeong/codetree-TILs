ff5f5b892a91188be346aff7bf8ac4f3137355d0
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-09-30 12:28:13 +0900

    Title: 아름다운 수열 2, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250930/아름다운 수열 2/beautiful-sequence-2.py
@@ -0,0 +1,22 @@
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+# Please write your code here.
+# 길이가 m인 a의 부분이 b의 원소 순서 바꾼거에 해당하는지
+# 1. 길이가 m인 a의 부분 만들기
+# 2. b의 원소 모두 통과하는지 -> 하나라도 아니라면 false
+    # 카운팅
+#3. 카운팅 값 출력
+
+cnt=0
+for start_idx in range(N-M+1): #시작인덱스값 : 0 ~ (n-m)
+        part = A[start_idx:start_idx+M]
+        isValid = True
+        for i in range(M):
+            if not B[i] in part:
+                isValid = False
+        if isValid:
+            cnt+=1
+
+print(cnt)
