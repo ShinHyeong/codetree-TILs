3b84ad87fb77e9a5b1f289a0cc5d5ad0a35925dd
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-10-04 23:09:46 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 199ms, Memory: 23MB, Status: Passed - Codetree

---
+++ 251004/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -0,0 +1,27 @@
+N = int(input())
+a1, b1, c1 = map(int, input().split())
+a2, b2, c2 = map(int, input().split())
+
+# Please write your code here.
+def dist(d1, d2):
+    return min(abs(d1-d2), N-max(d1,d2)+min(d1,d2))
+
+#1부터 N까지의 숫자를 이용해 총 3자리 만들기
+#1번째 조합에 대해 각자리 숫자와의 거리를 구하고
+    #만약 2를 넘으면 False로 설정한다
+#2번째 조합에 대해 각자리 숫자와의 거리를 구하고
+    #만약 2를 넘으면 False로 설정한다
+#둘중 하나가 True이면 카운팅한다
+cnt=0
+for i in range(1,N+1):
+    for j in range(1,N+1):
+        for k in range(1,N+1):
+            isOpen1 = False
+            isOpen2 = False
+            if dist(a1,i)<=2 and dist(b1,j)<=2 and dist(c1,k)<=2:
+                isOpen1 = True
+            if dist(a2,i)<=2 and dist(b2,j)<=2 and dist(c2,k)<=2:
+                isOpen2 = True
+            if isOpen1 or isOpen2:
+                cnt+=1
+print(cnt)
