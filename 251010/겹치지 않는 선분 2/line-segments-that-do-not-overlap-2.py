b7b38d807aa4c923f3cf2d028d6bbb315f349bf1
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-10-10 12:37:28 +0900

    Title: 겹치지 않는 선분 2, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251010/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 251010/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -6,20 +6,19 @@ x1 = [line[0] for line in lines]
 x2 = [line[1] for line in lines]
 
 #하나의 선분을 고르고
-#나머지 선분의 위치를 하나씩 확인하면서 겹치는지 확인한다
-#겹친다면 그 수를 카운팅한다
-#(전체 선분 갯수 - 겹치는 선분 갯수) 출력
+#나머지 선분의 위치를 하나씩 확인하면서 겹치는 선분인지 확인한다
+#겹치지 않는 선분이라면 카운팅한다
 
-dotInLine = [0] * n #라인마다 겹치는 점의 갯수를 기록한다
+answer = 0 #겹치지 않는 선분의 갯수
 for i in range(n): #하나의 선분을 고르고
+    overlap = False
     for j in range(i+1, n):
-        #겹치는가 : 겹치는 특징에 해당된다면 카운팅한다
+        #겹치는가 : 겹치는 특징에 해당된다면 세지 않는다
         if (x1[i]<=x1[j] and x2[j]<=x2[i]) or (x1[j]<=x1[i] and x2[i]<=x2[j]):
-            dotInLine[i]+=1
-            dotInLine[j]+=1
+            overlap = True
+            break
 
-cntLine=0 #겹치는 라인의 갯수
-for cntDot in dotInLine:
-    if cntDot>=1:
-        cntLine+=1
-print(n-cntLine)
+    if overlap == False: #선분 i는 겹치지 않는 선분이구나
+        answer +=1
+
+print(answer)
