613ba0b04b1f2828154dbc9f8abc60eac40f13ea
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-10-10 11:19:50 +0900

    Title: 삼각형 만들기, Time: 88ms, Memory: 18MB, Status: Passed - Codetree

--- 251010/삼각형 만들기/create-triangle.py
+++ 251010/삼각형 만들기/create-triangle.py
@@ -8,12 +8,26 @@ y = [p[1] for p in points]
 max_val = 0
 # N개의 점 중 3개 고르기
 for i in range(n):
-    for j in range(i+1, n):
-        for k in range(j+1, n):
-            #최대넓이의 2배 구하기 : 최대x x 최대y
-            area = max(x[i],x[j],x[k]) * max(y[i],y[j],y[k])
-            
+    for j in range(n):
+        for k in range(n):
+            #3개의 점은 모두 다른 점이다
+            if i==j or i==k or j==k:
+                continue
+
+            #삼각형을 만들 수 있는 필수조건: 
+            #dot1,dot2,dot3를 골랐다고 쳤을 때, dot3 = (dot1의 x좌표, dot2의 y좌표)
+            if not(x[k]==x[i] and y[k]==y[j]):
+                continue
+
+            #최대넓이의 2배 구하기
+            max_x = max(x[i],x[j],x[k])
+            min_x = min(x[i],x[j],x[k])
+            max_y = max(y[i],y[j],y[k])
+            min_y = min(y[i],y[j],y[k])
+            area = (max_x-min_x) * (max_y-min_y)
+            #print("x",max_x, "y",max_y)
             #최댓값인지 확인 
             max_val = max(area, max_val)
 
+#삼각형을 만들 수 없다면 0 출력
 print(max_val)
