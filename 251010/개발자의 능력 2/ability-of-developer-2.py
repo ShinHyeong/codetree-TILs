02b6648dc70dad24d5e702438bcc5ae80529b2ed
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-10-10 09:58:54 +0900

    Title: 개발자의 능력 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251010/개발자의 능력 2/ability-of-developer-2.py
+++ 251010/개발자의 능력 2/ability-of-developer-2.py
@@ -5,13 +5,12 @@ ability = list(map(int, input().split()))
 # 총합이 가장 큰 팀 가장 작은 팀의 차 구하기
 # 그 차가 최솟값인지 확인
 # 차의 최솟값 출력
-# 0 1 2 3 4 5
 min_val = 1000000
 for x1 in range(len(ability)):
     for x2 in range(x1+1, len(ability)):
         for x3 in range(len(ability)):
             for x4 in range(x3+1, len(ability)):
-                if x1==x3:
+                if x1==x3 or x2==x4:
                     continue
 
                 sum1 = ability[x1]+ability[x2]
