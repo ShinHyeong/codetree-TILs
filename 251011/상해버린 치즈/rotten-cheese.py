2e560df80b06ae7e445461e1763a66763fb023b0
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-10-11 02:10:04 +0900

    Title: 상해버린 치즈, Time: 59ms, Memory: 17MB, Status: Passed - Codetree

--- 251011/상해버린 치즈/rotten-cheese.py
+++ 251011/상해버린 치즈/rotten-cheese.py
@@ -18,11 +18,14 @@ for _ in range(S):
 #상한 치즈 후보인지 확인 : 아픈사람 모두가 해당치즈를 아프기 전에 먹었나?
 def is_bad(cheese_idx):
     for i in range(S): #아픈사람에 대해
-        sick_idx = sick_p[i]
-        for j in range(D): #치즈먹은기록에서 아픈사람이 해당치즈를 아프기전에 먹었는지 체크한다 
-            if p[j]==sick_idx and m[j]==cheese_idx:
-                if t[j]>=sick_t[i]: #한명이라도 아니라면 상한치즈후보에서 제외된다
-                    return False
+        bad=False
+        for j in range(D): #치즈먹은기록에서 
+            if p[j]==sick_p[i] and m[j]==cheese_idx: #아픈사람이 해당치즈를 
+                if t[j]<sick_t[i]: #한번이라도 아프기전에 먹었는지 체크한다 
+                    bad = True
+                    break
+        if bad==False: #한명이라도 아니라면 상한치즈후보에서 제외된다
+            return False
     return True
 
 #치즈별로 먹은 시간을 확인한다
@@ -32,19 +35,18 @@ def is_bad(cheese_idx):
 max_val = 0 #아픈사람 수 최댓값 = 필요한 약의 갯수 최댓값
 
 for cheese_idx in range(1,M+1): #치즈별로 검사하자. 치즈는 1번부터 M번까지이다.
-    cnt = 0
-
-    if is_bad(cheese_idx) == False: #상한치즈가 아니라면 카운팅하지 않는다
+    
+    if is_bad(cheese_idx) == False: #상한치즈후보가 아니라면 넘긴다
         continue
 
-    sick_list = []
-    for k in range(D): #기록을 하나씩 보는데
-        if m[k]==cheese_idx: #상한 치즈를 먹은 사람이라면
-            if p[k] in sick_list: #단, 이미 카운팅된 사람은 세지않음
+    maybe_sick = []
+    for i in range(D): #해당치즈를 먹은기록에 대해
+        if m[i]==cheese_idx:
+            if p[i] in maybe_sick: #단 이미 카운팅된 환자는 환자후보에서 제외한다 (중복제거)
                 continue
-            sick_list.append(p[k]) #카운팅
-    
+            maybe_sick.append(p[i]) #환자후보 추가(카운팅)
+
     #최대로 아픈 사람 수인지 확인
-    max_val = max(len(sick_list),max_val)
+    max_val = max(len(maybe_sick),max_val)
 
 print(max_val)
