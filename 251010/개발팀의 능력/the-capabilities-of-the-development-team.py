0677e7c4582dc1b729ca1013ee0d5c24e42dbfa8
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-10-10 10:12:46 +0900

    Title: 개발팀의 능력, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251010/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -0,0 +1,26 @@
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+
+min_val = 1000
+for a1 in range(len(arr)):
+    for a2 in range(a1+1, len(arr)):
+        
+        for a3 in range(len(arr)):
+            for a4 in range(a3+1, len(arr)):
+                
+                if not(a1!=a3 and a1!=a4 and a2!=a3 and a2!=a4): #필수전제조건 : 팀원은 모두 인덱스가 다르다 
+                    continue
+                
+                sum1 = arr[a1]+arr[a2]
+                sum2 = arr[a3]+arr[a4]
+                sum3 = sum(arr)-sum1-sum2
+
+                if sum1==sum2 or sum2==sum3 or sum1==sum3: #모든 팀의 능력치는 서로 다르게 묶여야함
+                    continue
+
+                diff = max(sum1, sum2, sum3) - min(sum1, sum2, sum3)
+
+                min_val = min(diff, min_val)
+
+print(-1 if min_val==1000 else min_val)
