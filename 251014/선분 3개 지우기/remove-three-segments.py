2c8dfca50d931a29174ba7ea323b08bc6ff44bef
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-10-14 01:17:44 +0900

    Title: 선분 3개 지우기, Time: 61ms, Memory: 17MB, Status: Passed - Codetree

--- 251014/선분 3개 지우기/remove-three-segments.py
+++ 251014/선분 3개 지우기/remove-three-segments.py
@@ -7,36 +7,29 @@ for _ in range(n):
     r.append(right)
 
 # Please write your code here.
-#주어진 직선을 표시한다
-MAX_INT = 100
-arr = [0] * (MAX_INT+1)
-for i in range(len(l)):
-    left,right = l[i], r[i]
-    for k in range(left, right+1):
-        arr[k] += 1
 
-def delete_line(arr, line_idx):
-    left, right = l[line_idx], r[line_idx]
-    for i in range(left, right+1):
-        arr[i] -= 1
-
-#서로 다른 선분 3개를 고른다
-#제거해본다
+#서로 다른 선분 3개를 골라서 그 선분을 제외하고 그린다
 #남은 n-3개의 선분끼리 서로 겹치지 않는지 확인한다
     #겹치지 않는다면 카운팅
 cnt=0 
 for l1_idx in range(n): #서로 다른 선분 3개 고르기
     for l2_idx in range(l1_idx+1, n):
         for l3_idx in range(l2_idx+1, n):
-            #제거해본다
-            arr_cpy = arr.copy()
-            delete_line(arr_cpy, l1_idx)
-            delete_line(arr_cpy, l2_idx)
-            delete_line(arr_cpy, l3_idx)
+            
+            MAX_INT = 100
+            arr = [0] * (MAX_INT+1)
+
+            for i in range(len(l)):
+                if i==l1_idx or i==l2_idx or i==l3_idx: #그 선분을 제외하고 그린다
+                    continue
+
+                left,right = l[i], r[i]
+                for k in range(left, right+1):
+                    arr[k] += 1
 
             overlap = False
-            for i in range(len(arr_cpy)):
-                if arr_cpy[i]>1: #한번이라도 겹치면 True
+            for i in range(len(arr)):
+                if arr[i]>1: #한번이라도 겹치면 True
                     overlap = True
             if overlap == False:
                 cnt+=1
