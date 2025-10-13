b2c63ca5ed2bbc51563d4e25e36732cc6af409d3
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-10-14 00:47:42 +0900

    Title: 이상한 폭탄 2, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251014/이상한 폭탄 2/strange-bomb-2.py
+++ 251014/이상한 폭탄 2/strange-bomb-2.py
@@ -6,7 +6,7 @@ num = [int(input()) for _ in range(N)]
 #폭발된 폭탄이 가장 큰 번호인지 확인
 #가장 큰 번호 출력
 
-max_num = 0 #가장 큰 번호 : 폭탄이 가질 수 있는 번호의 최솟값으로 초기화
+max_num = -1 
 
 for target in range(len(num)):
     for i in range(target+1, len(num)):
@@ -14,8 +14,3 @@ for target in range(len(num)):
             max_num = max(num[target], max_num)
 
 print(max_num)
-
-#[7,3,4,2,3,4,8,4,6,7]
-#[7,4,2,4,8,4,6,7] #3폭발
-#[7,2,8,6,7] #4폭발
-#
