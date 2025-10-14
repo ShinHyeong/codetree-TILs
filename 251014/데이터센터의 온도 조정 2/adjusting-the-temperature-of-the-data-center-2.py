85fb30026367592cf4318f4e23888c53c7fd9cdc
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-10-14 16:49:19 +0900

    Title: 데이터센터의 온도 조정 2, Time: 52ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 251014/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.py
@@ -0,0 +1,19 @@
+N, C, G, H = map(int, input().split())
+ranges = [tuple(map(int, input().split())) for _ in range(N)]
+
+# Please write your code here.
+def process(t, ta, tb):
+    if t<ta:
+        return C
+    elif ta<=t and t<=tb:
+        return G
+    elif t>=tb:
+        return H
+
+max_val = 0
+for t in range(1002): #적정 온도
+    sum_val = 0
+    for ta, tb in ranges:
+        sum_val += process(t, ta, tb)
+    max_val = max(sum_val, max_val)
+print(max_val)
