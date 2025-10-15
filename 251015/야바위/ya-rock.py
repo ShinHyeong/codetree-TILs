ada0af198bb62fd72064e833f875683cf7d2252c
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-10-15 11:50:06 +0900

    Title: 야바위, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 251015/야바위/ya-rock.py
+++ 251015/야바위/ya-rock.py
@@ -4,7 +4,7 @@ a, b, c = zip(*moves)
 a, b, c = list(a), list(b), list(c)
 
 # Please write your code here.
-scores = [0]*3 #종이컵마다 점수를 저장한다
+max_val = 0
 for target in range(3): #조약돌을 넣을 종이컵 num
     cups = [False]*3
     cups[target] = True #조약돌을 넣는다
@@ -21,9 +21,7 @@ for target in range(3): #조약돌을 넣을 종이컵 num
         if cups[c_idx]:
             score+=1
     
-    #점수를 저장한다
-    scores[target] = score
+    #점수가 최댓값인지 확인한다
+    max_val = max(score, max_val)
 
-for i in range(3):
-    if scores[i]==max(scores):
-        print(i+1)
+print(max_val)
