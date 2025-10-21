5ef09951ea04919b44cff85e8a5a2f23ec1f1a03
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-10-22 04:13:38 +0900

    Title: 팀으로 하는 틱택토 2, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251022/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 251022/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -1,85 +1,67 @@
-MAX_A = 3
-MAX_X = 9
+inp = [input() for _ in range(3)]
 
-board = [
-    list(map(int, input()))
-    for _ in range(MAX_A)
-]
+# Please write your code here.
+board = [list(map(int, i)) for i in inp]
+
+def is_win(cnt_p1, cnt_p2):
+    return (cnt_p1==2 and cnt_p2==1) or (cnt_p1==1 and cnt_p2==2)
+
+# 가로 / 세로 / 대각선으로 같은수2개 다른수1개 조합이 몇개?
+PLAYER_MIN_NUM, PLAYER_MAX_NUM = 1, 9
 
 ans = 0
 
-# 모든 쌍에 대해 전부 시도해 봅니다.
-for i in range(1, MAX_X + 1):
-    for j in range(i + 1, MAX_X + 1):
-        # i, j 두 명이 팀이 됐을 때 이길 수 있는지를 확인합니다.
-        # win : 두 명이 팀이 됐을 때 이길 수 있다면 true
-        win = False
+#승리한 플레이어 조합(팀)을 만든다 (player1 != player2)
+for player1 in range(PLAYER_MIN_NUM, PLAYER_MAX_NUM+1):
+    for player2 in range(player1+1, PLAYER_MAX_NUM+1):
+        
+        #그 팀이 승리하는 경우 중 하나라도 해당하면 카운팅하고 종료
 
-        # num_i , num_j : 각 줄에 i, j가 나오는 개수
-        num_i = 0
-        num_j = 0
+        win = False #현재 승리한 상태인지
 
-        # 가로로 빙고가 만들어질 때
-        for k in range(MAX_A):
-            num_i = 0
-            num_j = 0
-            for l in range(MAX_A):
-                if board[k][l] == i:
-                    num_i += 1
-                if board[k][l] == j:
-                    num_j += 1
-
-            # 3개의 칸에 i, j가 총 3번 나오며
-            # 둘 다 최소 1번 이상 나오면 승리
-            if num_i + num_j == 3 and num_i >= 1 and num_j >= 1:
+        #가로로 빙고가 만들어지는지 검사
+        for i in range(3):
+            cnt_p1, cnt_p2 = 0,0
+            for j in range(3):
+                if board[i][j]==player1:
+                    cnt_p1+=1
+                if board[i][j]==player2:
+                    cnt_p2+=1
+            if is_win(cnt_p1, cnt_p2):
                 win = True
 
-        # 세로로 빙고가 만들어질 때
-        for k in range(MAX_A):
-            num_i = 0
-            num_j = 0
-            for l in range(MAX_A):
-                if board[l][k] == i:
-                    num_i += 1
-                if board[l][k] == j:
-                    num_j += 1
-
-            # 3개의 칸에 i, j가 총 3번 나오며
-            # 둘 다 최소 1번 이상 나오면 승리
-            if num_i + num_j == 3 and num_i >= 1 and num_j >= 1:
+        #세로로 빙고가 만들어지는지 검사
+        for j in range(3):
+            cnt_p1, cnt_p2 = 0,0
+            for i in range(3):
+                if board[i][j]==player1:
+                    cnt_p1+=1
+                if board[i][j]==player2:
+                    cnt_p2+=1
+            if is_win(cnt_p1, cnt_p2):
                 win = True
 
-        # 왼쪽 위에서 오른쪽 아래를 잇는 대각선으로 빙고가 만들어질 때
-        num_i = 0
-        num_j = 0
-        for k in range(MAX_A):
-            if board[k][k] == i:
-                num_i += 1
-            if board[k][k] == j:
-                num_j += 1
-
-        # 3개의 칸에 i, j가 총 3번 나오며
-        # 둘 다 최소 1번 이상 나오면 승리
-        if num_i + num_j == 3 and num_i >= 1 and num_j >= 1:
+        #좌측 상단 -> 우측 하단 방향의 대각선으로 빙고가 만들어지는지 검사
+        cnt_p1, cnt_p2 = 0,0
+        for k in range(3):
+            if board[k][k]==player1:
+                cnt_p1+=1
+            if board[k][k]==player2:
+                cnt_p2+=1
+        if is_win(cnt_p1, cnt_p2):
             win = True
 
-
-        # 오른쪽 위에서 왼쪽 아래를 잇는 대각선으로 빙고가 만들어질 때
-        num_i = 0
-        num_j = 0
-        for k in range(MAX_A):
-            if board[k][MAX_A - k - 1] == i:
-                num_i += 1
-            if board[k][MAX_A - k - 1] == j:
-                num_j += 1
-
-        # 3개의 칸에 i, j가 총 3번 나오며
-        # 둘 다 최소 1번 이상 나오면 승리
-        if num_i + num_j == 3 and num_i >= 1 and num_j >= 1:
+        #우측 하단 -> 좌측 상단 방향의 대각선으로 빙고가 만들어지는지 검사
+        cnt_p1, cnt_p2 = 0,0
+        for k in range(3):
+            if board[k][3-k-1]==player1:
+                cnt_p1+=1
+            if board[k][3-k-1]==player2:
+                cnt_p2+=1
+        if is_win(cnt_p1, cnt_p2):
             win = True
-
 
         if win:
-            ans += 1
+            ans +=1
 
 print(ans)
