e1852f59493918a1cd0199b3e644ecb189108ec2
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-10-22 03:29:05 +0900

    Title: 팀으로 하는 틱택토 2, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251022/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 251022/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -1,46 +1,85 @@
-inp = [input() for _ in range(3)]
+MAX_A = 3
+MAX_X = 9
 
-# Please write your code here.
-def in_range(i,j):
-    return (0<=i and i<3) or (0<=j and j<3)
+board = [
+    list(map(int, input()))
+    for _ in range(MAX_A)
+]
 
-def is_same2diff1(d1,d2,d3):
-    return (d1==d2 and d1!=d3) or (d1==d3 and d1!=d2) or (d2==d3 and d2!=d1)
+ans = 0
 
-# 가로 / 세로 / 대각선으로 같은수2개 다른수1개 조합이 몇개?
-answers = []
+# 모든 쌍에 대해 전부 시도해 봅니다.
+for i in range(1, MAX_X + 1):
+    for j in range(i + 1, MAX_X + 1):
+        # i, j 두 명이 팀이 됐을 때 이길 수 있는지를 확인합니다.
+        # win : 두 명이 팀이 됐을 때 이길 수 있다면 true
+        win = False
 
-#가로로 검사
-for i in range(3):
-    d1,d2,d3 = inp[i][0],inp[i][1],inp[i][2]
-    if is_same2diff1(d1,d2,d3):
-        tmp = [d1,d2,d3]
-        tmp.sort()
-        if (d1,d3) not in answers:
-            answers.append((d1,d3))
+        # num_i , num_j : 각 줄에 i, j가 나오는 개수
+        num_i = 0
+        num_j = 0
 
-#세로로 검사
-for j in range(3):
-    d1,d2,d3 = inp[0][j],inp[1][j],inp[2][j]
-    if is_same2diff1(d1,d2,d3):
-        tmp = [d1,d2,d3]
-        tmp.sort()
-        if (d1,d3) not in answers:
-            answers.append((d1,d3))
+        # 가로로 빙고가 만들어질 때
+        for k in range(MAX_A):
+            num_i = 0
+            num_j = 0
+            for l in range(MAX_A):
+                if board[k][l] == i:
+                    num_i += 1
+                if board[k][l] == j:
+                    num_j += 1
 
-#대각선으로 검사
-d1,d2,d3 = inp[0][0],inp[1][1],inp[2][2]
-if is_same2diff1(d1,d2,d3):
-    tmp = [d1,d2,d3]
-    tmp.sort()
-    if (d1,d3) not in answers:
-        answers.append((d1,d3))
+            # 3개의 칸에 i, j가 총 3번 나오며
+            # 둘 다 최소 1번 이상 나오면 승리
+            if num_i + num_j == 3 and num_i >= 1 and num_j >= 1:
+                win = True
 
-d1,d2,d3 = inp[0][2], inp[1][1], inp[2][0]
-if is_same2diff1(d1,d2,d3):
-    tmp = [d1,d2,d3]
-    tmp.sort()
-    if (d1,d3) not in answers:
-        answers.append((d1,d3))
+        # 세로로 빙고가 만들어질 때
+        for k in range(MAX_A):
+            num_i = 0
+            num_j = 0
+            for l in range(MAX_A):
+                if board[l][k] == i:
+                    num_i += 1
+                if board[l][k] == j:
+                    num_j += 1
 
-print(len(answers))
+            # 3개의 칸에 i, j가 총 3번 나오며
+            # 둘 다 최소 1번 이상 나오면 승리
+            if num_i + num_j == 3 and num_i >= 1 and num_j >= 1:
+                win = True
+
+        # 왼쪽 위에서 오른쪽 아래를 잇는 대각선으로 빙고가 만들어질 때
+        num_i = 0
+        num_j = 0
+        for k in range(MAX_A):
+            if board[k][k] == i:
+                num_i += 1
+            if board[k][k] == j:
+                num_j += 1
+
+        # 3개의 칸에 i, j가 총 3번 나오며
+        # 둘 다 최소 1번 이상 나오면 승리
+        if num_i + num_j == 3 and num_i >= 1 and num_j >= 1:
+            win = True
+
+
+        # 오른쪽 위에서 왼쪽 아래를 잇는 대각선으로 빙고가 만들어질 때
+        num_i = 0
+        num_j = 0
+        for k in range(MAX_A):
+            if board[k][MAX_A - k - 1] == i:
+                num_i += 1
+            if board[k][MAX_A - k - 1] == j:
+                num_j += 1
+
+        # 3개의 칸에 i, j가 총 3번 나오며
+        # 둘 다 최소 1번 이상 나오면 승리
+        if num_i + num_j == 3 and num_i >= 1 and num_j >= 1:
+            win = True
+
+
+        if win:
+            ans += 1
+
+print(ans)
