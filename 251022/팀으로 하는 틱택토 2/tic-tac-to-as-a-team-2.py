927c2f5eb41697149d7e59987f27a373a2284d5b
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-10-22 03:26:35 +0900

    Title: 팀으로 하는 틱택토 2, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 251022/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 251022/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -1,27 +1,46 @@
 inp = [input() for _ in range(3)]
 
 # Please write your code here.
+def in_range(i,j):
+    return (0<=i and i<3) or (0<=j and j<3)
+
 def is_same2diff1(d1,d2,d3):
     return (d1==d2 and d1!=d3) or (d1==d3 and d1!=d2) or (d2==d3 and d2!=d1)
 
 # 가로 / 세로 / 대각선으로 같은수2개 다른수1개 조합이 몇개?
-ans=0
+answers = []
 
 #가로로 검사
 for i in range(3):
-    if is_same2diff1(inp[i][0],inp[i][1],inp[i][2]):
-        ans+=1
+    d1,d2,d3 = inp[i][0],inp[i][1],inp[i][2]
+    if is_same2diff1(d1,d2,d3):
+        tmp = [d1,d2,d3]
+        tmp.sort()
+        if (d1,d3) not in answers:
+            answers.append((d1,d3))
 
 #세로로 검사
 for j in range(3):
-    if is_same2diff1(inp[0][j],inp[1][j],inp[2][j]):
-        ans+=1
+    d1,d2,d3 = inp[0][j],inp[1][j],inp[2][j]
+    if is_same2diff1(d1,d2,d3):
+        tmp = [d1,d2,d3]
+        tmp.sort()
+        if (d1,d3) not in answers:
+            answers.append((d1,d3))
 
 #대각선으로 검사
-if is_same2diff1(inp[0][0],inp[1][1],inp[2][2]):
-    ans+=1
+d1,d2,d3 = inp[0][0],inp[1][1],inp[2][2]
+if is_same2diff1(d1,d2,d3):
+    tmp = [d1,d2,d3]
+    tmp.sort()
+    if (d1,d3) not in answers:
+        answers.append((d1,d3))
 
-if is_same2diff1(inp[0][2], inp[1][1], inp[2][0]):
-    ans+=1
+d1,d2,d3 = inp[0][2], inp[1][1], inp[2][0]
+if is_same2diff1(d1,d2,d3):
+    tmp = [d1,d2,d3]
+    tmp.sort()
+    if (d1,d3) not in answers:
+        answers.append((d1,d3))
 
-print(ans)
+print(len(answers))
