61a9e8d5c43bae3865ea1561f1b6e3845a937a73
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-10-23 01:44:51 +0900

    Title: 원소 값들의 최대 합, Time: 52ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 251023/원소 값들의 최대 합/maximum-sum-of-element-values.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+arr = [0] + list(map(int, input().split()))
+
+# Please write your code here.
+
+max_val = 0
+for i in range(n): #시작위치를 고른다
+    sum_val = 0 #시작위치 i에 대한, m번의 움직임에 대한 누적합
+    for _ in range(m): #움직임은 M번 반복한다
+        elm = arr[i]
+        i = elm
+        sum_val += arr[elm] #원소들의 누적합을 구한다
+    max_val = max(sum_val, max_val) #시작위치 i에 대한 원소들의 누적합이 최대인지 확인한다
+print(max_val)
