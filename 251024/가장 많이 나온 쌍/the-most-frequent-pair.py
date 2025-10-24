427b7695baf6ed3b1118c9fb7944900abef8eb43
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-10-24 15:36:56 +0900

    Title: 가장 많이 나온 쌍, Time: 53ms, Memory: 16MB, Status: Passed - Codetree

--- 251024/가장 많이 나온 쌍/the-most-frequent-pair.py
+++ 251024/가장 많이 나온 쌍/the-most-frequent-pair.py
@@ -4,11 +4,11 @@ pairs = [tuple(map(int, input().split())) for _ in range(m)]
 # Please write your code here.
 
 max_cnt = 0
-for a_tmp in range(1,n+1): #
+for a_tmp in range(1,n+1): #후보들을 만든다
     for b_tmp in range(1,n+1):
-        cnt_tmp = 0
-        for a,b in pairs:
+        cnt_tmp = 0 
+        for a,b in pairs: #해당 후보가 pair를 만족하는 갯수를 구한다
             if (a_tmp==a and b_tmp==b) or (a_tmp==b and b_tmp==a):
                 cnt_tmp+=1
-        max_cnt = max(cnt_tmp,max_cnt)
+        max_cnt = max(cnt_tmp,max_cnt) #그 해당 후보에 일치하는 갯수가 최댓값인지 확인한다 
 print(max_cnt)
