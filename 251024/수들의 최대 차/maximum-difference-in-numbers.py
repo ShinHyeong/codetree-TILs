deca5f4ae477af12423d25be23a292a5472a3c75
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-10-24 16:05:39 +0900

    Title: 수들의 최대 차, Time: 130ms, Memory: 17MB, Status: Passed - Codetree

--- 251024/수들의 최대 차/maximum-difference-in-numbers.py
+++ 251024/수들의 최대 차/maximum-difference-in-numbers.py
@@ -1,30 +1,16 @@
-MAX_NUM = 10000
+N, K = map(int, input().split())
+arr = [int(input()) for _ in range(N)]
 
-# 선언 및 입력:
-n, k = tuple(map(int, input().split()))
-arr = [
-    int(input())
-    for _ in range(n)
-]
+# Please write your code here.
 
-# 구간 [l, r] 
-# 사이에 들어있는 숫자 개수를 반환합니다.
-def count_num(l, r):
+# 최댓값과 최솟값의 차 구하는 문제 -> 최솟값를 고정시킨다
+# 최솟값이 a라고 할 때, 뽑은 숫자의 범위 : [a:a+k]
+
+max_cnt = 0
+for min_val in range(min(arr), max(arr)+1): #최솟값 고정
     cnt = 0
-    for elem in arr:
-        if l <= elem and elem <= r:
-            cnt += 1
-
-    return cnt
-
-
-ans = 0
-# 크기가 K인 모든 구간을 잡아
-# 해당 구간 안에 들어오는 숫자의 개수를 세서
-# 그 중 최댓값을 계산합니다.
-for i in range(1, MAX_NUM + 1):
-    # 구간 [i, i + k] 사이에 들어있는 숫자를 세어
-    # 최댓값을 계산합니다.
-    ans = max(ans, count_num(i, i + k))
-
-print(ans)
+    for elm in arr:
+        if (min_val<=elm and elm<=min_val+K):
+            cnt+=1
+    max_cnt = max(max_cnt, cnt)
+print(max_cnt)
