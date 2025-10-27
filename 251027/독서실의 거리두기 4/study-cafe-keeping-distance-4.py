188cc8f6d06a8fee11f78954d3ceb60db7046b3c
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-10-27 21:19:57 +0900

    Title: 독서실의 거리두기 4, Time: 75ms, Memory: 17MB, Status: Passed - Codetree

--- 251027/독서실의 거리두기 4/study-cafe-keeping-distance-4.py
+++ 251027/독서실의 거리두기 4/study-cafe-keeping-distance-4.py
@@ -6,15 +6,14 @@ seat = input()
 # 가장 가까운 두 사람의 거리 구하기
 # 최댓값인지 확인하기
 
-MAX_N = 100
-seat = list(map(int,seat))
+seat = list(seat)
 
 def get_min_dist():
 
-    min_dist = MAX_N
+    min_dist = N
     for i in range(N):
         for j in range(i+1, N):
-            if seat[i]==1 and seat[j]==1:
+            if seat[i]=='1' and seat[j]=='1':
                 min_dist = min(j-i, min_dist)
 
     return min_dist
@@ -24,11 +23,11 @@ for i in range(N):
     for j in range(i+1,N):
 
         #둘중하나라도 앉을수없다면 자리후보탈락
-        if seat[i]==1 or seat[j]==1:
+        if seat[i]=='1' or seat[j]=='1':
             continue
         
         #잠깐 자리 앉히기
-        seat[i],seat[j] = 1,1
+        seat[i],seat[j] = '1','1'
         
         #가장 가까운 두 사람의 거리 구하기
         min_dist = get_min_dist()
@@ -37,6 +36,6 @@ for i in range(N):
         max_val = max(max_val, min_dist)
 
         #원상복구
-        seat[i],seat[j] = 0,0
+        seat[i],seat[j] = '0','0'
 
 print(max_val)
