5ce2e0f077cf932ab7761a6445d7e6bb62edb9e7
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-11-07 01:48:57 +0900

    Title: 이상한 폭탄 3, Time: 57ms, Memory: 24MB, Status: Wrong Answer - Codetree

--- 251107/이상한 폭탄 3/strange-bomb-3.py
+++ 251107/이상한 폭탄 3/strange-bomb-3.py
@@ -3,42 +3,42 @@ num = [int(input()) for _ in range(N)]
 
 # Please write your code here.
 
-#가장 많이 터진 폭탄의 "번호"를 물어봄 -> 번호 단위로 완전탐색
+# 폭탄번호별로 터진횟수를 저장함
+MAX_NUM = max(num)
+explode_cnt = [0] * (MAX_NUM+1)
 
-unique_num = []
+# 주어진 폭탄이 터졌는지 안터졌는지 상태를 저장함
+explode = [False] * N
+
 for i in range(N):
-    if num[i] not in unique_num:
-        unique_num.append(num[i])
+    for j in range(i+1,N):
+        if i-j>K: #거리조건 만족하는지 확인
+            break #거리가 k를 초과하는 다음 j도 싹다 넘겨야 함
+        
+        if num[i]!=num[j]: #거리조건 만족하는데 폭탄번호가 다르면
+            continue #거리 만족하는 다음 j 케이스로 이동
+        
+        #이제 거리조건과 폭탄번호조건 만족함
+        #이미 터진 폭탄인지 상태를 확인하고
+            #안 터졌으면 터졌다는 상태를 기록한다
+            #터진횟수를 카운팅한다
 
-bomb_list = []
-for n in unique_num:
+        n1,n2 = num[i],num[j] #조건을 만족하는 폭탄번호
+        if explode[i]==False:
+            explode[i]=True
+            explode_cnt[n1]+=1
+        
+        if explode[j]==False:
+            explode[j]=True
+            explode_cnt[n2]+=1
 
-    #해당 번호의 폭탄 터졌는지 확인
-    is_bomb = [False]*N
-    for i in range(N):
-        for j in range(i+1,N):
-            if num[i]==n and num[j]==n and j-i<=K:
-                is_bomb[i], is_bomb[j] = True,True
 
-    #그 번호의 터진 폭탄 갯수 세기
-    bomb_cnt = 0
-    for b in is_bomb:
-        if b==True:
-            bomb_cnt +=1
-    
-    #저장 (폭탄번호, 터진횟수)
-    bomb_list.append((n, bomb_cnt))
-
-#가장 많이 터진 횟수 구하기
-max_bomb_cnt = 0
-for n,bomb_cnt in bomb_list:
-    max_bomb_cnt = max(max_bomb_cnt, bomb_cnt)
-
+# 가장 많이 터진 횟수를 구하고
 #그 횟수에 해당하는 폭탄번호 최댓값 구하기
+max_explode_cnt = 0
 max_bomb_num = 0
-for n,bomb_cnt in bomb_list:
-    if bomb_cnt == max_bomb_cnt:
-        max_bomb_num = max(n, max_bomb_num)
-
-#아무 폭탄도 터지지 않을 경우 0 출력
-print(max_bomb_num if max_bomb_cnt!=0 else 0)
+for n,cnt in enumerate(explode_cnt):
+    if cnt>=max_explode_cnt:
+        max_explode_cnt = cnt
+        max_bomb_num = n
+print(max_bomb_num)
