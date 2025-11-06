a78b8f2181b30ab9e992ca3d7f48ef66bb070ea1
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-11-07 04:03:11 +0900

    Title: 언덕 깎기, Time: 84ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 251107/언덕 깎기/hill-cutting.py
+++ 251107/언덕 깎기/hill-cutting.py
@@ -10,18 +10,11 @@ for h in range(MIN_H,MAX_H+1): #최솟값 고정
     cost = 0
 
     for j in range(N):
-        if h==heights[j]:
-            x=0
-
-        if heights[j]<=h:
+        if heights[j]<h:
             x = h-heights[j] #h를 최솟값으로 만들어야함 : h_j+x = h
 
-        if heights[j]>h:
-            if abs(heights[j]-h)<=17:
-                x=0
-
-            if heights[j]>h+17: #마지노선보다 크다면 최댓값이 마지노선이 되어야하므로
-                x = heights[j]-(h+17) #h_j - x = h+17 
+        if heights[j]>h+17: #마지노선보다 크다면 최댓값이 마지노선이 되어야하므로
+            x = heights[j]-(h+17) #h_j - x = h+17 
         
         cost += x*x #비용계산
     
