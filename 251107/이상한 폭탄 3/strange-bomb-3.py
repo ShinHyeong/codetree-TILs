7d51aa9493af7b9d46292c20475c2e46e0da4d72
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-11-07 00:45:34 +0900

    Title: 이상한 폭탄 3, Time: 55ms, Memory: 17MB, Status: Passed - Codetree

--- 251107/이상한 폭탄 3/strange-bomb-3.py
+++ 251107/이상한 폭탄 3/strange-bomb-3.py
@@ -8,22 +8,42 @@ num = [int(input()) for _ in range(N)]
 #N크기의 폭탄리스트를 만들고 터진 갯수를 저장한다
 #가장 많이 터진 폭탄의 번호를 출력한다
 
-bomb = [False] * N
+#가장 많이 터진 폭탄의 "번호"를 물어봄 -> 번호 단위로 완전탐색
 
+unique_num = []
 for i in range(N):
-    for j in range(i+1,N):
-        if (j-i)<=K:
-            bomb[i],bomb[j] = True,True
+    if num[i] not in unique_num:
+        unique_num.append(num[i])
 
-MAX_NUM = max(num)
-bomb_cnt = [0] * (MAX_NUM+1)
-for pos in range(N):
-    if bomb[pos]:
-        n = num[pos] #해당 위치의 폭탄 숫자를 넘겨주고
-        bomb_cnt[n] += 1 #그 숫자 갯수를 센다
+bomb_list = []
+for n in unique_num:
 
-max_bomb_i = 0
-for i,cnt in enumerate(bomb_cnt):
-    if cnt == max(bomb_cnt):
-        max_bomb_i = max(max_bomb_i, i)
-print(max_bomb_i)
+    #해당 번호의 폭탄 터졌는지 확인
+    is_bomb = [False]*N
+    for i in range(N):
+        for j in range(i+1,N):
+            if num[i]==n and num[j]==n and j-i<=K:
+                is_bomb[i], is_bomb[j] = True,True
+
+    #그 번호의 터진 폭탄 갯수 세기
+    bomb_cnt = 0
+    for b in is_bomb:
+        if b==True:
+            bomb_cnt +=1
+    
+    #저장 (폭탄번호, 터진횟수)
+    bomb_list.append((n, bomb_cnt))
+
+#가장 많이 터진 횟수 구하기
+max_bomb_cnt = 0
+for n,bomb_cnt in bomb_list:
+    max_bomb_cnt = max(max_bomb_cnt, bomb_cnt)
+
+#그 횟수에 해당하는 폭탄번호 최댓값 구하기
+max_bomb_num = 0
+for n,bomb_cnt in bomb_list:
+    if bomb_cnt == max_bomb_cnt:
+        max_bomb_num = max(n, max_bomb_num)
+
+#아무 폭탄도 터지지 않을 경우 0 출력
+print(max_bomb_num if max_bomb_cnt!=0 else 0)
