c07516fce9935e7fd5f4cff6badea88fb2962671
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-11-07 03:40:22 +0900

    Title: 언덕 깎기, Time: 76ms, Memory: 17MB, Status: Passed - Codetree

--- 251107/언덕 깎기/hill-cutting.py
+++ 251107/언덕 깎기/hill-cutting.py
@@ -2,26 +2,26 @@ N = int(input())
 heights = [int(input()) for _ in range(N)]
 
 # Please write your code here.
-MAX_H = 100
-min_cost = MAX_H * MAX_H
+MIN_H,MAX_H = 1,100
+min_cost = (MAX_H-MIN_H)*(MAX_H-MIN_H)*N
 
-for i in range(N): #최솟값 고정
+for h in range(MIN_H,MAX_H+1): #최솟값 고정
     
     cost = 0
 
     for j in range(N):
-        if heights[i]==heights[j]:
+        if h==heights[j]:
             x=0
 
-        if heights[j]<=heights[i]:
-            x = heights[i]-heights[j] #h_i를 최솟값으로 만들어야함 : h_j+x = h_i
+        if heights[j]<=h:
+            x = h-heights[j] #h를 최솟값으로 만들어야함 : h_j+x = h
 
-        if heights[j]>heights[i]:
-            if abs(heights[j]-heights[i])<=17:
+        if heights[j]>h:
+            if abs(heights[j]-h)<=17:
                 x=0
 
-            if heights[j]>heights[i]+17: #마지노선보다 크다면 최댓값이 마지노선이 되어야하므로
-                x = heights[j]-(heights[i]+17) #h_j - x = h_i+17 
+            if heights[j]>h+17: #마지노선보다 크다면 최댓값이 마지노선이 되어야하므로
+                x = heights[j]-(h+17) #h_j - x = h+17 
         
         cost += x*x #비용계산
     
