a161d36d343a97f438abb53721c789f465ac21a6
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-11-07 03:23:19 +0900

    Title: 언덕 깎기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251107/언덕 깎기/hill-cutting.py
@@ -0,0 +1,31 @@
+N = int(input())
+heights = [int(input()) for _ in range(N)]
+
+# Please write your code here.
+MAX_H = 100
+min_cost = MAX_H * MAX_H
+
+for i in range(N): #최솟값 고정
+    
+    cost = 0
+
+    for j in range(N):
+        if heights[i]==heights[j]:
+            x=0
+
+        if heights[j]<=heights[i]:
+            x = heights[i]-heights[j] #h_i를 최솟값으로 만들어야함 : h_j+x = h_i
+
+        if heights[j]>heights[i]:
+            if abs(heights[j]-heights[i])<=17:
+                x=0
+
+            if heights[j]>heights[i]+17: #마지노선보다 크다면 최댓값이 마지노선이 되어야하므로
+                x = heights[j]-(heights[i]+17) #h_j - x = h_i+17 
+        
+        cost += x*x #비용계산
+    
+    print(heights[i],cost)
+    min_cost = min(cost, min_cost) #최소비용인지 확인
+
+print(min_cost)
