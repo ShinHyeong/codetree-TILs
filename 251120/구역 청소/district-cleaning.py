0b47a78a1411e41072112577633887467f73c7c7
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-11-20 00:33:07 +0900

    Title: 구역 청소, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251120/구역 청소/district-cleaning.py
+++ 251120/구역 청소/district-cleaning.py
@@ -7,13 +7,15 @@ c, d = map(int, input().split())
 
 #1. 겹치지 않는 경우 : (d-c)+(b-a)
 #2. 겹치는 경우
-#1) c<=b and b<=d
-#2) a<=c and c<=b
-
+#1) 맨앞이 a, 맨뒤가 d : d-a
+#2) 맨앞이 a, 맨뒤가 b : b-a
+#3) 맨앞이 c, 맨뒤가 b : b-c
 if b<c or d<a: 
     print((d-c)+(b-a))
 else:
-    if c<=b and b<=d:
+    if a<=c and c<=b and b<=d:
         print(d-a)
-    else:
+    if a<=c and d<=b:
+        print(b-a)
+    if c<=a and a<=d and d<=b:
         print(b-c)
