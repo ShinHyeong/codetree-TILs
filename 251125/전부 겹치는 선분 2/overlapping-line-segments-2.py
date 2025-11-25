e381dca579db41782c5e5c3698de3711ff820df3
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-11-25 23:49:08 +0900

    Title: 전부 겹치는 선분 2, Time: 54ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251125/전부 겹치는 선분 2/overlapping-line-segments-2.py
@@ -0,0 +1,30 @@
+n = int(input())
+segments = [tuple(map(int, input().split())) for _ in range(n)]
+x1 = [seg[0] for seg in segments]
+x2 = [seg[1] for seg in segments]
+
+# Please write your code here.
+
+#[접근방법]
+#하나의 선분을 제외하고
+#시작점에서의 가장 큰 값 (max_x1)
+#끝점에서의 가장 작은 값(min_x2)을 구한다
+# 모든 선분이 다 겹치는 조건인지 확인한다 (max_x1 <= min_x2)
+MIN_X,MAX_X = 1,100
+
+
+all_overlap = False
+for e in range(n): #제외할 선분 하나를 뽑는다
+
+    max_x1,min_x2 = MIN_X, MAX_X
+    for i in range(n): #그 선분을 제외한 max_x1, min_x2을 구한다
+        if i==e:
+            continue
+        max_x1 = x1[i] if max_x1 <= x1[i] else max_x1
+        min_x2 = x2[i] if min_x2 >= x2[i] else min_x2
+
+    if max_x1 <= min_x2:
+        all_overlap = True
+        break
+
+print("Yes" if all_overlap else "No")
