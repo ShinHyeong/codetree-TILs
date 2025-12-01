0f7ac6edf3f74774a00347b5c07029bb8a2850f9
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-12-02 00:47:10 +0900

    Title: 달리자, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251202/달리자/run-and-run.py
+++ 251202/달리자/run-and-run.py
@@ -1,27 +1,17 @@
+# 가장 세련된 O(N) 풀이 (누적합 방식)
 n = int(input())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
-# Please write your code here.
+ans = 0
+current_diff = 0 # 현재 누적해서 넘겨받은(또는 줘야할) 사람 수
 
-# 1번 집부터 보면서 남으면 가장 가까운 부족한 집에게 나눠준다
-
-dst = 0
 for i in range(n):
-    if A[i]>B[i]: #남는집이라면
-        for j in range(i+1,n): 
-            if A[j]<B[j]: #부족한 집을 찾아
-                #부족한 만큼 나눠준다
-                #그런데 만약 부족한 양 >= 남는 양이라면 남는 양을 전부줘야한다
-                if B[j]-A[j] >= A[i]-B[i]:
-                    give = A[i]-B[i]
-                    A[i] -= give
-                    A[j] += give
-                    dst += (give) * (j-i)
-                #남는양 > 부족한 양이라면 부족한 양만큼만 주면 된다
-                else:
-                    give = B[j]-A[j]
-                    A[i] -= give
-                    A[j] += give
-                    dst += (give) * (j-i)
-print(dst)
+    # (원래 있던 사람) - (있어야 할 사람)
+    # 양수면 남아서 옆으로 넘기는 거고, 음수면 부족해서 옆에서 받아야 함
+    current_diff += A[i] - B[i]
+    
+    # 옆 집으로 이동해야 하는 사람 수만큼 거리에 더함
+    ans += current_diff 
+
+print(ans)
