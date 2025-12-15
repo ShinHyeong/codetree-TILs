2adad1d708248108773c10d90eca0511554037ae
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2025-12-15 23:30:40 +0900

    Title: 연속된 숫자 만들기 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251215/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 251215/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -1,4 +1,19 @@
 pos = list(map(int, input().split()))
 
 # Please write your code here.
-print(2)
+
+# 이미 연속된 값인지 확인한다 -> 만약 그렇다면 print(0)
+#1번째 값이 2번쨰 값과 3번째 값 사이에 들어가면 연속된 값이 되는지 확인한다
+#  -> 만약 그렇다면 print(1)
+#3번째 값이 1번쨰 값과 2번째 값 사이에 들어가면 연속된 값이 되는지 확인한다
+#  -> 만약 그렇다면 print(1)
+#둘다 연속된 값이 안나오면 print(2)
+
+v1, v2, v3 = pos[0], pos[1], pos[2]
+
+if abs(v2-v1)==1 and abs(v3-v2)==1:
+    print(0)
+elif abs(v3-v2)==2 or abs(v2-v1)==2:
+    print(1)
+else:
+    print(2)
