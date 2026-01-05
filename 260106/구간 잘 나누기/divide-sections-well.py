b69e637c34da32533c048e7ed46b2cf68872d702
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2026-01-06 08:21:18 +0900

    Title: 구간 잘 나누기, Time: 59ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 260106/구간 잘 나누기/divide-sections-well.py
+++ 260106/구간 잘 나누기/divide-sections-well.py
@@ -3,19 +3,30 @@ a = list(map(int, input().split()))
 
 # Please write your code here.
 
-#만약 완전탐색으로 접근한다면 .. 
+#완전탐색 접근
 
 #구간을 나눈다
 #최댓값 구한다
 #최소인지 확인한다
 #이 과정을 모든 경우에 대해 반복한다
-#ㄴ하지만 모든 과정을 다 하려면 반복문 m번 써야함..
+#한계 : 그러나 for문의 횟수가 가변적이다 (for문 횟수 = m번)
 
-#더 작게 최댓값이 나오도록 구간을 나누려면 골고루 나누는게 중요하다
-num_ele = n//m
-sum_list = [0]*m
-j=0
-for i in range(m):
-    sum_list[i] = sum(a[j:j+num_ele])
-    j+=num_ele
-print(max(sum_list))
+def is_possible(limit): #limit : 최댓값
+    m_cnt = 1 #구간 갯수
+    curr_sum = 0
+
+    for ele in a:
+        if curr_sum + ele <= limit:
+            curr_sum += ele #계속 더함
+        else:
+            m_cnt+=1 #구간늘림
+            curr_sum = ele # 새 구간의 첫 숫자가 됨
+    
+    return m_cnt <= m
+
+answer = 100*(n-((m-1)*2))
+for limit in range(max(a)+1, sum(a)+1):
+    if is_possible(limit):
+        if limit <= answer:
+            answer = limit
+print(answer)
