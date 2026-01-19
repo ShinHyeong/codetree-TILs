2407407b36e00f6d93374f7152a72046c3279d1d
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2026-01-19 13:51:13 +0900

    Title: 최대 최소간의 차, Time: 65ms, Memory: 17MB, Status: Passed - Codetree

--- 260119/최대 최소간의 차/difference-between-maximum-and-minimum.py
+++ 260119/최대 최소간의 차/difference-between-maximum-and-minimum.py
@@ -3,45 +3,27 @@ arr = list(map(int, input().split()))
 
 # Please write your code here.
 
-# 6 3 7 3 5 최대 최소 차가 2 이하가 되게
-# 현재의 max를 깎거나 현재의 min을 높이거나
+# 모든 구간의 수의 범위 : 1이상 10000 이하로 매우작음
+# 모든 수는 [x, x+k] 여야함
+# x를 1부터 10000까지 다 움직여보며
+# 모든 수를 그 구간안에 끼워넣었을 때의 비용을 구한다
+# 그중 최소를 리턴한다
 
-def is_diff_LTE_K(arr):
-    return max(arr)-min(arr) <= k
-
-def get_min_val_cnt(arr):
-    min_val, min_val_cnt = min(arr), 0
+# [x,x+k] 사이로 바꾸는 cost를 계산한다
+def get_cost(low, high):
+    cost = 0
     for ele in arr:
-        if ele == min_val:
-            min_val_cnt += 1
-    return min_val_cnt
-
-def get_max_val_cnt(arr):
-    max_val, max_val_cnt = max(arr), 0
-    for ele in arr:
-        if ele == max_val:
-            max_val_cnt += 1
-    return max_val_cnt
-
-cost = 0
-min_val, max_val = min(arr), max(arr)
-
-while not is_diff_LTE_K(arr):
-    if get_max_val_cnt(arr) < get_min_val_cnt(arr):
-        max_val -= 1
-        for i in range(n):
-            if arr[i] == max_val+1:
-                cost += 1
-                arr[i] -= 1
-
-    if is_diff_LTE_K(arr):
-        break
-
-    if get_min_val_cnt(arr) <= get_max_val_cnt(arr):
-        min_val += 1
-        for i in range(n):
-            if arr[i] == min_val-1:
-                cost += 1
-                arr[i] += 1
+        if ele < low:
+            cost += (low-ele)
+        if ele > high:
+            cost += (ele-high)
     
-print(cost)
+    return cost
+
+# 모든 구간별 비용을 구해보며 그 중 최솟값을 계산한다
+min_cost = 10000 * 100
+
+for num in range(1,10000+1):
+    min_cost = min(min_cost, get_cost(num,num+k))
+
+print(min_cost)
