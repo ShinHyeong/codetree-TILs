1eaa29e735c62dc2ce0bb40bc719fda9bd595163
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2026-01-30 02:33:45 +0900

    Title: 최소 와이파이 수, Time: 86ms, Memory: 19MB, Status: Runtime Error - Codetree

--- 260130/최소 와이파이 수/minimum-number-of-wifi.py
+++ 260130/최소 와이파이 수/minimum-number-of-wifi.py
@@ -10,20 +10,30 @@ arr = list(map(int, input().split()))
 ## 예제 분석 & 로직 설정 ##
 #   v     v   
 # 1 1 0 1 1 1
+#   v           v
+# 1 1 1 0 0 0 1 1 1
 # m=1이라면 자신을 포함해 양옆 1+1명까지 커버가능 (=3명)
 # 즉 1+m+m명까지 커버 가능하다는 것이다
-# 따라서 n//(1+m+m) 하면 될듯
-# 만약 나누어떨어지지 않으면 (n//(1+m+m)) + 1
-people_cnt = 0
-for ele in arr:
-    if ele == 1:
-        people_cnt +=1
+# 일단 앞에서부터 (1+m+m)명을 봤을 때, 1이 하나라도 있으면 카운팅해야함
 
-#answer= n//(1+m+m)
-#if n%(1+m+m)!=0:
-#    answer = (n//(1+m+m)) + 1
+answer = 0
 
-answer= people_cnt//(1+m+m)
-if people_cnt%(1+m+m)!=0:
-    answer = (people_cnt//(1+m+m)) + 1
+group_cnt = 1+m+m #5
+start_idx = 0
+#print("start_idx", start_idx)
+while True:
+    
+    for i in range(start_idx, start_idx+group_cnt): #0부터 4까지
+        if arr[i]==1:
+            answer += 1
+            break
+
+    start_idx += group_cnt #5부터 시작
+    
+    #print("start_idx", start_idx)
+
+    if n > start_idx >= (n-group_cnt): #11-5=6
+        answer += 1
+        break
+
 print(answer)
