ee6690f0b7faffc6192fc56b4944ab344fdcd1b0
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2026-01-30 02:12:39 +0900

    Title: 최소 와이파이 수, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260130/최소 와이파이 수/minimum-number-of-wifi.py
@@ -0,0 +1,21 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+
+## 요구사항 분석 ##
+# 모든 사람이 사용할 수 있도록 와이파이 최소한으로 설치
+# 와이파이수 리턴
+
+## 예제 분석 & 로직 설정 ##
+#   v     v   
+# 1 1 0 1 1 1
+# m=1이라면 자신을 포함해 양옆 1+1명까지 커버가능 (=3명)
+# 즉 1+m+m명까지 커버 가능하다는 것이다
+# 따라서 n//(1+m+m) 하면 될듯
+# 만약 나누어떨어지지 않으면 (n//(1+m+m)) + 1
+
+answer= n//(1+m+m)
+if n%(1+m+m)!=0:
+    answer = (n//(1+m+m)) + 1
+print(answer)
