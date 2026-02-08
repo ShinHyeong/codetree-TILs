51d4588859422fb88dbb2d6d9c5dfb330eaaa7e7
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2026-02-08 18:34:59 +0900

    Title: 수열의 순서 바꾸기, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 260208/수열의 순서 바꾸기/reorder-sequence.py
+++ 260208/수열의 순서 바꾸기/reorder-sequence.py
@@ -7,27 +7,20 @@ sequence = list(map(int, input().split()))
 # 맨앞 숫자를 수열중간에 집어넣는다
 # 최소 몇 번 반복해야 오름차순 정렬이 되는가
 
-## 예제분석 ##
-# 1 2 4 3
-# 2 4 3 -> 1을 2뒤, 4뒤, 3뒤에 집어넣을 수 있다
-# 2 4 1 3 -> 4뒤 pick cnt=1
-# 4 1 3 -> 2를 4뒤, 1뒤, 3뒤에 집어넣을 수 있다
-# 4 1 2 3 -> 1뒤 pick cnt=2
-# 1 2 3 -> 4를 1뒤 2뒤 3뒤에 집어넣을 수 있다
-# 1 2 3 4 -> 3뒤 pick cnt=3
 
 ## 로직 설정 ##
-# 순환 구조
-# 맨 앞 숫자가 a라면 a-1뒤에 집어넣으면 된다
-# (단, 맨 앞 숫자가 1이라면 N뒤에 집어넣는다)
-cnt = 0
-sorted_seq = sorted(sequence)
-while True:
-    if sequence==sorted_seq:
-        break
-    target = sequence[0]-1 if sequence[0]!=1 else n
-    target_idx = sequence.index(target)
-    sequence = sequence[1:target_idx+1]+[sequence[0]]+sequence[target_idx+1:]
-    cnt+=1
+# 맨뒤에서 봤을 때 이미 오름차순으로 잘 정리된 곳은 건들지 않는다
+# 35 50 4 39 54 32 20 53 29 18 55 28 40 44 48 25 21 5 9 14 33 3 16 38 31 23 15 60 27 49 11 51 46 52 30 45 1 6 36 43 24 7 58 56 42 2 8 10 12 13 17 19 22 26 34 37 41 47 57 59
+# 이 수열에서 [2 8 10 12 13 17 19 22 26 34 37 41 47 57 59] 이 부분은 건들지 않는다. 그 앞 부분 수들의 정렬이 이상하니까 옮겨준다.
 
-print(cnt)
+
+# 이미 잘 정리된 인덱스 시작점 찾기
+good_start_idx = -1
+for i in range(n-1,-1,-1):
+    if sequence[i]<sequence[i-1]:
+        good_start_idx = i
+        break
+
+# sequence[0:good_start_idx-1]는 어차피 이동해야할 숫자
+# 그 숫자의 갯수를 센다
+print(good_start_idx)
