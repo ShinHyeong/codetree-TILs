820004d077e7ca7038181c014a547fe63d852dea
Shin-Hyeong, Park <shinhyeong.dev@gmail.com>
2026-02-18 01:34:33 +0900

    Title: 황금비율 토스트, Time: 79ms, Memory: 19MB, Status: Runtime Error - Codetree

--- 260218/황금비율 토스트/golden-toast.py
+++ 260218/황금비율 토스트/golden-toast.py
@@ -1,23 +1,18 @@
 n, m = map(int, input().split())
 s = input()
-s = list(s)
-arrow = len(s)
-#print(arrow, s)
+
+stack_l = list(s)
+stack_r = []
 
 for _ in range(m):
     cmd = input().split()
     if cmd[0] == "L":
-        if arrow > 0:
-            arrow -= 1
+        stack_r.append(stack_l.pop())
     if cmd[0] == "R":
-        if arrow < len(s):
-            arrow += 1
+        stack_l.append(stack_r.pop())
     if cmd[0] == "D":
-        if arrow < len(s):
-            s = s[:arrow]+s[arrow+1:]
+        stack_r.pop()
     if cmd[0] == "P":
-        to_add = cmd[1]
-        s = s[:arrow]+[to_add]+s[arrow:]
-        arrow += 1
-    #print(arrow, s)
-print("".join(s))
+        stack_l.append(cmd[1])
+
+print("".join(stack_l)+"".join(reversed(stack_r)))
